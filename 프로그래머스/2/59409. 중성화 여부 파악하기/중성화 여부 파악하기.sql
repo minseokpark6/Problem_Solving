014d029fb04a3c52c81b9053a5e78d525da7a109
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2024-04-14 08:11:08 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,8 @@
+select ANIMAL_ID, NAME,
+    case
+        when SEX_UPON_INTAKE like "Neutered%" then 'O'
+        when SEX_UPON_INTAKE like "Spayed%" then 'O'
+        else 'X'
+    end as 중성화
+from ANIMAL_INS
+order by 1;
