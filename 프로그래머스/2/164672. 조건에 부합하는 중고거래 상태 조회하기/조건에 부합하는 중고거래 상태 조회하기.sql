4769b7f32ee66c98f81ec3a6ed191e520401f3c8
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2024-04-18 08:39:37 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,9 @@
+select BOARD_ID, WRITER_ID, TITLE, PRICE,
+        case STATUS
+        when "SALE" then "판매중"
+        when "RESERVED" then "예약중"
+        when "DONE" then "거래완료"
+        end as STATUS
+from USED_GOODS_BOARD
+where CREATED_DATE like "2022-10-05%"
+order by 1 desc;
