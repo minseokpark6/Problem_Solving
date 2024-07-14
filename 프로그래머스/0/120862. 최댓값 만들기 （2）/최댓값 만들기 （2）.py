477041760752707931c5368a4fa91ccbfd9b2e3f
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2024-07-14 12:59:13 +0900

    Update 최댓값 만들기 （2）.py

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -7,3 +7,17 @@ def solution(numbers):
             li.append(mul)
     answer = max(li)
     return answer
+
+'''
+def solution(numbers):
+    answer = 0
+    for idx1 in range(len(numbers)-1):
+        for idx2 in range(idx1+1, len(numbers)):
+            mul = numbers[idx1] * numbers[idx2]
+            if mul > answer :
+                answer = mul
+    return answer
+'''
+# 테스트 7 통과 X 
+# 모든 mul 값이 음수가 나올 경우도 고려해야함
+# 부등호는 음수의 최대값을 표현하기 부적합
