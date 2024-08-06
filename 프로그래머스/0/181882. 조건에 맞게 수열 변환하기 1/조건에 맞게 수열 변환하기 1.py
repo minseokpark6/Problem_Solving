fb1caa4b7d2defa357ffba85b84e04a807f62aea
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2024-08-07 07:48:49 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 122.10 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer = []
+    for i in arr:
+        if i >= 50 and i % 2 == 0:
+            i = i / 2
+        elif i < 50 and i % 2 == 1 :
+            i *= 2
+        else: 
+            pass
+        answer.append(i)
+    return answer
