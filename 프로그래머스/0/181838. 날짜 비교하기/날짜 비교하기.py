74dfd0bb143ae6cee39a4457f2c58486ef8cc2a0
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2024-08-12 10:20:02 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -0,0 +1,20 @@
+def solution(date1, date2):
+    if date1 < date2: 
+        return 1
+    else:
+        return 0
+
+
+"""
+# 리스트 간 크기 비교
+
+0번 index부터 index 간 크기 비교 가능
+리스트의 길이에 상관없음
+
+ex. 
+a = [1, 2, 4]
+b = [1, 2, 3, 7]
+
+a > b : True
+
+"""
