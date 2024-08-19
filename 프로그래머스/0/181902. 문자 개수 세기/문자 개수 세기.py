06287d233826a2effff577078094cbb382703368
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2024-08-19 12:22:06 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -0,0 +1,7 @@
+def solution(my_string):
+    al = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    answer = [0] * len(al)
+    for idx, a in enumerate(al):
+        if a in my_string:
+            answer[idx] += my_string.count(a)
+    return answer
