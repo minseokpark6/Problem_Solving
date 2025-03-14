676a6698b8372e5660563e346c63947e0e808a21
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-03-14 12:37:11 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,6 @@
+n, m = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(n)]
+B = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n):
+    print(" ".join(str(A[i][j] + B[i][j]) for j in range(m)))
