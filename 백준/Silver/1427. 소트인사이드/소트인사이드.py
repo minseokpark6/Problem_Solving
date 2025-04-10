6deaeead7b4b1706c48485b7cd499a8a674c41e4
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-04-10 14:08:30 +0900

    [Silver V] Title: 소트인사이드, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -0,0 +1,9 @@
+# 입력 및 출력
+print(int("".join((sorted(input(), reverse=True)))))
+
+'''
+# 입력
+arr = sorted([int(i) for i in input()], reverse=True)
+# 출력 
+print(int("".join([str(i) for i in arr])))
+'''
