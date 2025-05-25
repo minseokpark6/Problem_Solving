49db7fabc7424c9fcb0536066e950956b2cd22b5
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-05-25 16:49:23 +0900

    [Bronze III] Title: 팩토리얼, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -0,0 +1,16 @@
+import math
+# 입력
+n = int(input())
+# 출력
+print(math.factorial(n))
+
+
+''' cf. for문을 활용하여 구하기 
+# 입력
+n = int(input())
+# 팩토리얼 구하기
+answer = 1
+for i in range(10, 0, -1):
+    answer *= i
+# 출력
+print(answer)'''
