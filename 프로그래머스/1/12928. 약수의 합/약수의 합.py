cbc2d9f38342e7485d59a76b270910e59b74d81d
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-09-06 20:55:26 +0900

    [level 1] Title: 약수의 합, Time: 0.28 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,7 +1,13 @@
 def solution(n):
+    return sum([i for i in range(1, n+1) if n%i == 0])
+
+
+''' 이전 코드
+def solution(n):
     result = []
     for i in range(1, n+1):
         if n % i == 0:
             result.append(i)
     answer = sum(result)
     return answer
+'''
