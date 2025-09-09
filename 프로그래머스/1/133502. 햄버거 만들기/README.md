e57c052d77e2a2aa1641ed20f4780623604ca030
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-09-09 22:11:05 +0900

    [level 1] Title: 햄버거 만들기, Time: 185.67 ms, Memory: 24.1 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/README.md
+++ 프로그래머스/1/133502. 햄버거 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 햄버거 만들기 - 133502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502) 
 
 ### 성능 요약
 
-메모리: 26.7 MB, 시간: 129.51 ms
+메모리: 24.1 MB, 시간: 185.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 02일 14:28:38
+2025년 09월 08일 22:10:55
 
 ### 문제 설명
 
--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,3 +1,31 @@
+from collections import deque
+
+def solution(ingredient):
+    # 변수 지정 
+    cnt = 0 
+    stack = deque()
+
+    # 재료의 경우의 수 확인
+    for i in ingredient:
+        stack.append(i)
+    
+        # 햄버거를 만들 수 있는 경우의 수인지 확인
+        if i == 1 and len(stack) >= 4:
+            if stack[-2] == 3 and stack[-3] == 2 and stack[-4] == 1:
+                cnt += 1
+                for i in range(4):
+                    stack.pop()
+
+    # 출력
+    return cnt
+
+"""
+1) deque를 활용하여 삽입/삭제 안정적으로 O(1), 대량 데이터에 성능개선 
+    -> 슬라이싱은 불가능
+'''
+
+
+''' 이전 코드
 def solution(ingredient):
     # 변수 지정
     answer = 0
@@ -14,8 +42,6 @@ def solution(ingredient):
     # 출력
     return answer
 
-
-"""
+>>>
 stack 사용하여 재료 순서대로 확인
-
 """
