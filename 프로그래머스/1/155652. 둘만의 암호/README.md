127ef842af9ad7a0c79d622532a7b00a6e91029b
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-09-11 22:47:27 +0900

    [level 1] Title: 둘만의 암호, Time: 0.03 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/README.md
+++ 프로그래머스/1/155652. 둘만의 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 둘만의 암호 - 155652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.03 ms
+메모리: 9.28 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 01일 17:41:36
+2025년 09월 10일 22:47:17
 
 ### 문제 설명
 
--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -1,6 +1,39 @@
 def solution(s, skip, index):
-    answer = ''
+    
     # 영문 소문자 리스트 생성 
+    alph = [chr(i) for i in range(97, 123) if chr(i) not in skip]
+    length = len(alph)
+    
+    # 소문자 인덱스 -> 딕셔너리로 매핑
+    char_to_idx = {char:i for i, char in enumerate(alph)}
+
+    # 문자 변환 
+    answer = []
+    for ch in s:
+        idx = (char_to_idx[ch]+index)%length
+        answer.append(alph[idx])
+
+    # 출력
+    return "".join(answer)
+
+
+'''
+## 수정 사항 
+1) list.index() >> 매 번 index를 찾기 위해 리스트의 길이만큼 돌아야 함 
+>> 딕셔너리로 해결 
+
+2) circle 사용 
+>> alph의 길이와 나머지를 이용하여 불필요한 연산 및 실수 제거
+
+
+'''
+
+
+''' >> 기존 코드 
+
+def solution(s, skip, index):
+    answer = ''
+    # 영문 소문자 리스트 생성 // skip에 들어가있는 문자 제외
     alph = [chr(i) for i in range(97, 123)]
     # skip에 들어가 있는 문자 제거 
     for sk in skip:
@@ -16,6 +49,7 @@ def solution(s, skip, index):
 
     # 출력
     return answer
+'''
 
 
 """
