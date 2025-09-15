4a77fc64b56d3da5021f2ac18b5d909cc89478af
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-09-15 23:10:04 +0900

    [level 1] Title: 과일 장수, Time: 84.77 ms, Memory: 20.7 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,4 +1,25 @@
 def solution(k, m, score):
+    # 변수 설정 
+    answer = 0
+
+    # 내름차순 정렬 
+    score.sort(reverse=True)
+
+    # 박스 별 점수 카운트 
+    # >> 내림차순 정렬로 m번째 마다 최소값 자리에 해당
+    for i in range(m-1, len(score), m):
+        answer += score[i]*m
+
+    # 출력
+    return answer
+
+'''
+>> 기존 코드 대비 개선점
+1) min() 호출로 인한 O(m) -> O(1)로 최적화 
+2) 슬라이싱 제거 
+
+
+def solution(k, m, score):
     # 변수 설정
     answer = 0
     box_count = len(score) // m
@@ -13,3 +34,6 @@ def solution(k, m, score):
     
     # 출력
     return answer
+
+
+'''
