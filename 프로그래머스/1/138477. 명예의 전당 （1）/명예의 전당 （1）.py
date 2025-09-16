f575c2fe0e3ff6b09bdd2babd073a4dfe6bb32d2
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-09-16 20:39:52 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.99 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,17 +1,21 @@
-def solution(k, score):
-    # 변수 지정
-    answer = []
+def solution(k, score): 
+    # 변수 지정 
+    result = []
     hall_of_fame = []
-    
-    # 발표 점수 확인
-    for i in score:
-        hall_of_fame.append(i)
-        hall_of_fame.sort(reverse=True)
-        if len(hall_of_fame) <= k:
-            answer.append(hall_of_fame[-1])
+
+    # 발표된 최하위 점수 구하기 
+    for s in score:
+        # 명예의 전당에 점수 추가
+        if len(hall_of_fame) >= k:
+            if hall_of_fame[-1] < s:
+                hall_of_fame.pop()
+                hall_of_fame.append(s)
         else:
-            hall_of_fame.pop()
-            answer.append(hall_of_fame[-1])
-            
+            hall_of_fame.append(s)
+        
+        # 최하위 점수 발표 
+        hall_of_fame.sort(reverse=True)
+        result.append(hall_of_fame[-1])
+
     # 출력
-    return answer
+    return result
