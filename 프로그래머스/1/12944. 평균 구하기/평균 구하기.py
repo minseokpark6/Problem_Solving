a774fad64c83327fdc13fedbf0e7e72c7528ba55
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-09-22 23:25:25 +0900

    [level 1] Title: 평균 구하기, Time: 0.00 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.py
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.py
@@ -1,3 +1,23 @@
 def solution(arr):
+    return sum(arr) / len(arr)
+
+''' 
+파이썬에서 평균을 구하는 가장 효율적인 방법 
+
+(1) sum(arr) / len(arr)
+
+(2) numpy 활용
+numpy.mean(arr)
+
+-> 대용량 데이터에 더 적합한 방법 
+-> 데이터 용량이 적을 경우 오히려 import하는 메모리가 더 소모됨.
+'''
+
+
+'''
+## 이전 코드 
+
+def solution(arr):
     answer = sum(arr) / len(arr)
     return answer
+'''
