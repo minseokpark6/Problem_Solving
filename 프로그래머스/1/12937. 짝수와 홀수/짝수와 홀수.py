29c21b4e86daf6f2ece0a7db28a50a7c62681c69
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-09-22 23:21:04 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.00 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.py
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.py
@@ -1,5 +1,10 @@
 def solution(num):
+    return "Even" if num%2 == 0 else "Odd"
+
+''' > 이전 코드 
+def solution(num):
     if num % 2 == 0:
         return "Even"
     else:
         return "Odd"
+'''
