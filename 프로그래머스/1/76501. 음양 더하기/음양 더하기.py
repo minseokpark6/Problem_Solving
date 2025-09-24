2db1ccfe7f30501d8f9c3b743ddf1b4d9bfb56b6
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-09-24 09:27:40 +0900

    [level 1] Title: 음양 더하기, Time: 0.20 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,4 +1,17 @@
 def solution(absolutes, signs):
+    # 변수 정의 
+    result = 0
+    # 정수의 합 구하기
+    for idx, n in enumerate(absolutes):
+        result += n if signs[idx] == True else (-n)
+    # 출력
+    return result
+
+''' 이전 코드 
+>> 리스트 사용 불필요
+- 메모리 문제 
+
+def solution(absolutes, signs):
     result = []
     for idx, n in enumerate(absolutes):
         if signs[idx] == True:
@@ -7,3 +20,4 @@ def solution(absolutes, signs):
             result.append(-n)
     answer = sum(result)
     return answer
+'''
