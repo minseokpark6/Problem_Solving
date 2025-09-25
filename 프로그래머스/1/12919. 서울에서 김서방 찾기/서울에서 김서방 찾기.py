839f93c4447d6b21b8775b0a634ea8a976af3fe2
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-09-25 21:28:18 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.01 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,4 +1,15 @@
 def solution(seoul):
+    # 김서방 위치 저장
     idx = seoul.index("Kim")
-    answer = f"김서방은 {idx}에 있다"
-    return answer
+    # 출력
+    return f"김서방은 {idx}에 있다"
+
+
+''' 
+- 딕셔너리나 추가 리스트를 사용하는 것이 오히려 메모리 낭비 
+- 이럴 경우엔 위의 코드처럼 바로 index를 사용하여 저정하는 것이 효율적 
+
+def solution(seoul):
+    dic = {name:idx for idx, name in enumerate(seoul) if name == "Kim"}
+    return f'감서방은 {dic["Kim"]}에 있다'
+'''
