2dda3967bda2a65843210e0ebc3bd29a27ffb58b
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-09-25 21:31:32 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 8.99 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,4 +1,12 @@
 def solution(s):
+    # 가운데 글자 인덱스 지정
+    idx = len(s)//2
+    # 출력
+    return s[idx] if len(s)%2 != 0 else s[idx-1:idx+1]
+
+''' >> 이전 코드 
+
+def solution(s):
     idx = len(s)//2
     # 홀수인 경우
     if len(s) % 2 != 0:
@@ -9,3 +17,4 @@ def solution(s):
     
     # 출력 
     return answer
+'''
