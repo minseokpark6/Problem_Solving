1f21387cb53f927e346ba70d4798d88ed8bc5e4e
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-09-25 21:39:44 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.26 ms, Memory: 15.1 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/README.md
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 제일 작은 수 제거하기 - 12935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12935#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12935) 
 
 ### 성능 요약
 
-메모리: 15.6 MB, 시간: 1.07 ms
+메모리: 15.1 MB, 시간: 2.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 06일 07:34:44
+2025년 09월 24일 21:39:37
 
 ### 문제 설명
 
--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -1,21 +1,20 @@
 def solution(arr):
+    # 배열의 길이가 1인 경우 
+    if len(arr) == 1:
+        return [-1]
+    # 그 외의 경우 
+    m = min(arr)
+    # 출력
+    return [i for i in arr if i != m]
+
+'''
+기존 코드 
+>> remove를 사용할 경우 원본 자체를 바꿔버림으로 비추천 
+
+def solution(arr):
     if len(arr) > 1:
         arr.remove(min(arr))
         return arr
     else:
         return [-1]
-
-
-
-
-"""
-# 주어진 순서가 바뀜... >> 틀림 
-
-def solution(arr):
-    result = sorted(arr)[::-1][:-1]
-    if len(result) > 0:
-        pass
-    elif len(result) == 0:
-        result.append(-1)
-    return result
-"""
+'''
