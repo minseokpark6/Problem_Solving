bfea2d75aae8a38d2eab6a7ceeb52107d60ed967
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-09-25 21:41:53 +0900

    [level 1] Title: 내적, Time: 0.15 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.py
+++ 프로그래머스/1/70128. 내적/내적.py
@@ -1,2 +1,15 @@
 def solution(a, b):
+    return sum(i*j for i, j in zip(a, b))
+
+'''
+(1) 제너레이터 표현식 vs 리스트 컴프리헨션
+- 굳이 리스트에 한 번 저장을 해서 sum에 보낼 필요가 없음 
+- 리스트를 만들지 않고, 하나씩 꺼내서 바로 sum에 전달
+- 메모리를 거의 쓰지 않고, 필요할 때마다 "즉석에서 생성"
+- 즉, 이터레이터를 만들어서 순차적으로 값을 생산
+'''
+
+'''
+def solution(a, b):
     return sum([i*j for i, j in zip(a, b)])
+'''
