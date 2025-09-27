745c48712023a052957c8bfdc48da4a7c9e5e946
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-09-28 07:39:40 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.16 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -1,3 +1,17 @@
 def solution(price, money, count):
+    total = sum(price*i for i in range(1, count+1))
+    return total - money if total > money else 0
+
+
+'''
+(1) 리스트 컴프리헨션 vs 제너레이터 표현식 
+- 불필요한 리스트 메모리 사용 X 
+
+(2) sum 연산을 굳이 반복할 필요 없음
+
+### 1차 코드 
+
+def solution(price, money, count):
     cost = [price*(i+1) for i in range(count)]
     return sum(cost) - money if sum(cost) > money else 0
+'''
