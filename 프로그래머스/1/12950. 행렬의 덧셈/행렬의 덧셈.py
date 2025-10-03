3681c859fc30a91e92e36da7dae88152238e163d
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-10-03 12:31:24 +0900

    [level 1] Title: 행렬의 덧셈, Time: 13.32 ms, Memory: 21.9 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,13 +1,24 @@
 def solution(arr1, arr2):
-    # 변수 정의 
-    answer = []
+    # 행렬의 덧셈 출력
+    return [[a + b for a, b in zip(i, j)] for i, j in zip(arr1, arr2)]
+
+'''
+# 변수 지정 
+answer = []
+# 행렬의 덧셈 계산 연산 
+for i, j in zip(arr1, arr2):
+    row = []
+    for a, b in zip(i, j):
+        row.append(a+b)
+    answer.append(row)
     
-    # 행렬의 덧셈 결과 구하기 
-    for i, j in zip(arr1, arr2):
-        temp = []
-        for idx in range(len(i)):
-            temp.append(i[idx]+j[idx])
-        answer.append(temp)
-        
-    # 출력
-    return answer
+print(answer)
+
+## 기존 코드 
+
+for i, j in zip(arr1, arr2):
+    temp = []
+    for idx in range(len(i)):
+        temp.append(i[idx]+j[idx])
+    answer.append(temp)
+'''
