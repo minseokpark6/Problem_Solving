5225ccea75060d32b27c5375cd6d5246ea139a54
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-10-12 19:29:52 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,4 +1,17 @@
 def solution(s):
+    return " ".join(w.capitalize() if w else w for w in s.split(" "))
+
+
+'''
+(1) capitalize 메소드 활용 
+- 단어의 첫 글자는 대문자로 나머지는 소문자로 변환해주는 파이썬 메소드 
+
+(2) 제너레이터 표현식 
+- 불필요한 리스트 사용을 없애서 메모리 절약
+
+### 이전 통과 코드
+
+def solution(s):
     # 모든 문자 소문자화
     s = s.lower()
     # 공백을 기준으로 분리
@@ -12,10 +25,10 @@ def solution(s):
     result = []
     
     for w in word: 
-        if i:
+        if w:
             z = w[0].upper() + w[1:]
         else:
-            z = i
+            z = w
         result.append(z)
     
     ## 파이썬에서 if i:는 i가 빈 문자열이 아닌 경우 참(True)으로 작동하고, i가 빈 문자열('')이면 거짓(False)으로 작동합
@@ -24,7 +37,7 @@ def solution(s):
     
     # 출력
     return " ".join(result)
-
+'''
 
 """
 >> 반례 : 스페이스가 두 개이상 나온 경우
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.02 ms
+메모리: 9.23 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 04일 17:49:43
+2025년 10월 11일 19:29:46
 
 ### 문제 설명
 
