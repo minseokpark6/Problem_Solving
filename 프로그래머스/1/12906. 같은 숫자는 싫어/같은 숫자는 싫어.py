b917a419c320f48b8faebf804531810b2aca46b2
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-10-13 12:29:34 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 64.99 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,4 +1,17 @@
 def solution(arr):
+    # 스택 생성 
+    stack = []
+    # 연속되지 않은 숫자만 스택에 추가
+    for i in arr:
+        # 스택이 비어있거나 숫자가 다른 경우에만 스택에 추가
+        if not stack or i != stack[-1]:
+            stack.append(i)
+    # 출력
+    return stack
+
+'''
+이전 통과 코드
+def solution(arr):
     answer = []
     # arr의 첫번째 원소값 추가
     answer.append(arr[0])
@@ -8,3 +21,4 @@ def solution(arr):
             answer.append(arr[idx])
     # 출력
     return answer
+    '''
