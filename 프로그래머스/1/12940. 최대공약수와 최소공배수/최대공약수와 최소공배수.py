4f5bf1b0b09f3e6d44696e033bf278735489306e
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-10-14 21:28:28 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.00 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,9 +1,31 @@
 def solution(n, m):
-    gcd = 1
-    for i in range(min(n, m), 0, -1):
-        if n % i == 0 and m % i == 0:
-            gcd = i 
-            break
-    answer = [gcd, (n*m)/gcd]
-    return answer
+    # 유클리드 호제법으로 최대공약수 구하기 
+    a, b = n, m
+    while b:
+        a, b = b, a%b
+    gcd = a
+    
+    # 최소공배수 = (n * m) // gcd
+    lcm = (n * m) // gcd
+    
+    # 출력
+    return [gcd, lcm]
 
+
+'''
+for 문으로 하나씩 비교해가면서 최대공약수를 찾는 방식보다 >> O(min(n, m))
+유클리드 호제법으로 최대공약수를 구하는 것이 시간복잡도를 줄일 수 있음 >> O(log(min(n, m)))
+
+
+## 기존 코드
+def solution(n, m):
+    # 변수 설정 -> 최대공약수 디폴트 값 
+    gcd = 1
+    # 최대공약수 구하기
+    for i in range(min(n,m), 0, -1):
+        if n%i == 0 and m%i==0:
+            gcd = i
+            break
+    # 출력 
+    return [gcd, (n*m)/gcd]
+'''
