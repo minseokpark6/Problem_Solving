2089ce5f9dd31d69387b473172c4e82addab6c6b
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-10-16 11:13:00 +0900

    [level 1] Title: 예산, Time: 0.52 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,14 +1,26 @@
+from itertools import accumulate 
+
+def solution(d, budget):
+    return sum(i <= budget for i in accumulate(sorted(d)))
+
+'''
+(1) 누적합 메소드를 활용하여 풀이
+
+## 이전 코드 
+
 def solution(d, budget):
     # 변수 정의
-    answer = 0
-    
-    # 예산 범위 내 지원 가능한 부서 찾기 
+    cnt = 0
+
+    # 예산 범위 내에서 최대로 지원가능한 부서의 수
     for i in sorted(d):
         if i <= budget:
-            answer += 1
-            budget -= i
+            cnt += 1 
+            budget -= i 
+        else:
+            break
         
-    # 출력           
-    return answer
+    # 출력
+    return cnt
 
-
+'''
