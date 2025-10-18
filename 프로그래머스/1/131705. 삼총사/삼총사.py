0947bc5584f74dda2aa9f0be23c08cc116089207
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-10-18 16:23:20 +0900

    [level 1] Title: 삼총사, Time: 0.07 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,5 +1,15 @@
-from itertools import combinations as comb
+from itertools import combinations
 
+def solution(number):
+    return sum(1 for triple in combinations(number, 3) if sum(triple)==0)
+
+
+'''
+(1) 리스트 컴프리헨션 vs 제너레이터 표현식 
+- 리스트 컴프리헨션 + count 는 불필요한 메모리 사용 및 중복 순회 발생 
+- 제너레이터 표현식 + sum()을 쓰면 한 번의 루프로 카운트 가능
+
+## 기존 코드
 def solution(number):
     # 주어진 리스트에서 세 개 수의 조합 리스트 
     result = [sum(c) for c in comb(number, 3)]
@@ -9,9 +19,9 @@ def solution(number):
     return answer
 
 
-"""
 # combinations 
 - itertools 모듈에 포함되어 있는 함수
 - 주어진 iterable에서 순서를 고려하지 않고 특정 길이의 모든 조합을 생성하는 데 사용
 - 조합을 나타내는 튜플을 반환
-"""
+
+'''
