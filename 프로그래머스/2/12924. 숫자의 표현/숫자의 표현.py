6b5363921bb96f1bc82b9da0ed49cc743fc19a90
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-10-20 19:33:08 +0900

    [level 2] Title: 숫자의 표현, Time: 1.61 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,14 +1,48 @@
 def solution(n):
-    # 변수 설정
-    answer = 0
-    # 연속한 자연수들로 n을 구하는 개수 구하기 
+    # 변수 정의 
+    cnt = 0
+    left, right, total = 1, 1, 1
+    
+    # 연속하는 자연수로 n이 표현되는 경우의 수
+    while left <= n:
+        if total < n:
+            right += 1
+            total += right
+    
+        elif total > n:
+            total -= left
+            left += 1
+
+        else:
+            cnt += 1
+            total -= left 
+            left += 1
+    
+    # 출력 
+    return cnt
+
+'''
+(1) 투 포인터(two-pointer)(슬라이딩 윈도우 방식)
+- 시간 복잡도 O(n²) -> O(n) 
+- 기존 모든 1부터 n까지 모든 숫자에서 합을 하나씩 확인하는 방식에서 불필요한 덧셈 반복을 제거한 방식
+
+
+## 이전 통과 코드
+def solution(n):
+    # 변수 정의
+    cnt = 0 
+    # 연속하는 자연수로 n이 표현되는 경우의 수
     for i in range(1, n+1):
-        sum = 0
+        sum = 0 
         num = i
+        
         while sum < n:
-            sum += num
+            sum += num 
             num += 1
+        
         if sum == n:
-            answer += 1
+            cnt += 1
     # 출력
-    return answer
+    return cnt
+    
+'''
