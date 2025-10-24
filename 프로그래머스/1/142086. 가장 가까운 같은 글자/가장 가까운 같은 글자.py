d3ca550b25b41219a8a75b70762ada066b2359f9
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-10-24 14:26:31 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.54 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,19 +1,36 @@
+
 def solution(s):
-    # 변수 설정
+    # 변수 설정 
     answer = []
-    dic = {}
+    last_index = {}
     
     # 문자열 위치 찾기 
-    idx = 0
-    for i in s:
+    for idx, i in enumerate(s):
+        answer.append(idx-last_index[i] if i in last_index else -1)
+        last_index[i] = idx
+        
+    return answer
+
+'''
+(1) if - else 문 구조 단순화 
+(2) 중복되는 딕셔너리 추가 부문 하나로 합침
+
+## 기존 코드 
+
+def solution(s):
+    # 변수 정의 
+    answer = []
+    dic = {}
+
+    # 문자열 위 찾기 
+    for idx, i in enumerate(s):
         if i in dic.keys():
             answer.append(idx - dic[i])
             dic[i] = idx
-            idx += 1
         else:
             answer.append(-1)
             dic[i] = idx
-            idx += 1
-            
+
     # 출력
     return answer
+'''
