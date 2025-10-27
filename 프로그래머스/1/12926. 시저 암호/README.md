7e6a41b68712d39281858039f3248be1ca37961c
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-10-27 16:26:15 +0900

    [level 1] Title: 시저 암호, Time: 1.97 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/README.md
+++ 프로그래머스/1/12926. 시저 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 시저 암호 - 12926 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 2.59 ms
+메모리: 9.42 MB, 시간: 1.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 01일 10:08:55
+2025년 10월 26일 16:26:10
 
 ### 문제 설명
 
--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,18 +1,42 @@
-def solution(s, n):
-    answer = ''
-    lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'] * 2
-    capital = [i.upper() for i in lower]
-    
-    for a in s:
-        if a.islower() :
-            idx = lower.index(a)
-            idx+=n
-            answer += lower[idx]
-        elif a.isupper() :
-            idx = capital.index(a)
-            idx+=n
-            answer += capital[idx]
-        elif a == " ":
-            answer += a
 
-    return answer
+def solution(s, n):
+    # 변수 정의 
+    result = []
+    
+    # 시저 암호화 
+    for c in s:
+        if c.isalpha():
+            result.append(
+                chr((ord(c) - ord('a')+n) % 26 + ord('a')) if c.islower() else chr((ord(c) - ord('A')+n) % 26 + ord('A'))
+            )
+        else:
+            result.append(c)
+
+    # 출력 
+    return "".join(result)
+
+''' 
+(1) list.index() => 매우 비효율적
+- 아스키 코드의 ord()를 활용하여 직접 계산 수행
+
+(2) 불필요한 문자열 리스트 사용 
+- 나머지 연산으로 불필요한 순환 피할 수 있음
+
+(3) 문자열 누적합 - 메모리로 인한 성능저하 
+- 리스트로 대체 후 "".join() 사용
+
+### 기존 통과 코드 
+
+def solution(s, n):
+    # 변수 정의 
+    result = ''
+    ch = [chr(i) for i in range(97, 123)] * 2
+    # 시저 암호화
+    for c in s:
+        if c == " ":
+            result += c
+        else:
+            result += ch[ch.index(c)+n] if c.islower() else ch[ch.index(c.lower())+n].upper()
+    # 출력
+    return result
+'''
