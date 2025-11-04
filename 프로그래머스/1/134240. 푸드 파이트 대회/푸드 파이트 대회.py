4f8a7a3cb151b89dad9cd27afea7f93e004004ae
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-11-04 22:54:38 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,13 +1,24 @@
 def solution(food):
+    # 왼쪽에서 먹는 선수의 음식
+    left = "".join(str(i) * (food[i] // 2) for i in range(1, len(food)))
+    
+    # 출력
+    return left + "0" + left[::-1]
+
+'''
+(1) 리스트 사용 X >> 문자열 바로 생성 
+(2) for 문에서 i를 하나씩 str 처리를 하던 것을 한 번에 진행 
+
+
+## 2차 통과 코드 
+def solution(food):
     # 왼 쪽에서 먹는 선수의 음식
     left = [str(i) for i in range(1, len(food)) for _ in range(food[i] // 2)]
     
     # 전체 음식 출력
     return "".join(left + ["0"] + left[::-1])
 
-'''
-
-## 이전 코드 
+## 1차 통과 코드 
 
 def solution(food):
     # 변수 지정
