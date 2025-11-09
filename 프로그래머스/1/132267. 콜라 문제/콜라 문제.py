9acc15596ec2fcaef8f70532da7e12dd62404219
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-11-09 13:05:16 +0900

    [level 1] Title: 콜라 문제, Time: 0.47 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,11 +1,12 @@
 def solution(a, b, n):
-    # 변수 지정
-    answer = 0
+    # 변수 정의 
+    cnt = 0 
     
-    # 받을 수 있는 콜라병 개수 카운트
+    # 받을 수 있는 콜라 개수 카운트 
     while n//a > 0:
-        answer += (n//a)*b
-        n = (n//a)*b + (n%a)
+        exchange = n//a
+        cnt += exchange*b
+        n = exchange*b + (n%a)
 
-    # 출력 
-    return answer
+    # 출력
+    return cnt
