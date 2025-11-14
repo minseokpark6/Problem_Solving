3fe2ce9e570d97f4777859fcab8472436590c285
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-11-15 08:54:36 +0900

    [level 1] Title: 카드 뭉치, Time: 0.89 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,19 +1,41 @@
+from collections import deque
+
 def solution(cards1, cards2, goal):
     # 변수 지정 
-    result = []
-    l1, l2 = len(cards1), len(cards2)
-    idx_1, idx_2 = 0, 0
+    q1 = deque(cards1)
+    q2 = deque(cards2)
     
     # 가능 여부 확인
-    for word in goal:
-        if idx_1 < l1 and word == cards1[idx_1]:
-            result.append(word)
-            idx_1 += 1
-        elif idx_2 < l2 and word == cards2[idx_2]:
-            result.append(word)
-            idx_2 += 1
+    for w in goal:
+        if q1 and q1[0] == w:
+            q1.popleft()
+        elif q2 and q2[0] == w:
+            q2.popleft()
         else:
-            return "No"
+            return 'No'
+    
+    # 출력
+    return 'Yes'
 
-    # 츌력  
-    return "Yes"
+
+''' 
+
+## 이전 통과 코드 
+
+def solution(cards1, cards2, goal):
+    # 변수 지정 
+    l1, l2 = len(cards1), len(cards2)
+    idx1, idx2 = 0, 0
+    
+    # 가능 여부 확인
+    for w in goal:
+        if (l1 > idx1) and (cards1[idx1] == w):
+            idx1 += 1
+        elif (l2 > idx2) and (cards2[idx2] == w):
+            idx2 += 1
+        else:
+            return 'No' 
+    
+    # 출력 
+    return 'Yes'
+'''
