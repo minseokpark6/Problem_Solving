9394dc8e1425f07278631a642009d84633e77482
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-11-17 23:44:16 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,20 +1,43 @@
-import math
+import math 
+
+def solution(arr):
+    # 변수 지정 
+    lcm = arr[0]
+
+    # N개의 최소공배수 찾기
+    if len(arr)>=2:
+        for num in arr[1:]:
+            lcm = lcm * num // math.gcd(lcm, num)
+    
+    # 출력
+    return lcm
+        
+
+'''
+(1) 매 반복마다 리스트 슬라이싱을 할 경우 성능이 좋지 않음 
+- 리스트 슬라이싱은 O(n)
+
+(2) 주어진 리스트를 계속 바꾸는 것 보다 누적 변수로 처리하는 것이 깔끔 
+
+
+## 기존 통과 코드 
+
+
+import math 
 
 def solution(arr):
     
-    while len(arr) >= 2:
-        # 배열 내 왼쪽 두 수 선택
+    # n개의 수의 최소 공배수 구하기 
+    while len(arr)>=2:
+        # 배열 내 왼쪽 두 개의 수 선택 
         a, b = arr[0], arr[1]
-        # 최소 공배수 배열에 추가
-        arr.append(a*b // math.gcd(a, b))
+        
+        # 두 수의 최소 공배수 배열에 추가
+        arr.append(a*b//math.gcd(a, b))
+        
         # 구한 숫자 제거 
         arr = arr[2:]
-
+    
     # 출력
     return arr[0]
-
-"""
-# 모든 수의 최소 공배수를 한 번에 구하는 것이 아니라 
-두 수의 최소 공배수를 리스트에 추가하여, 모든 수의 최소공배수를 구하는 방식으로
-
-"""
+'''
