3769eeaf54a80177b3833ebc56c1d1bab24663b2
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-11-22 11:36:17 +0900

    [level 1] Title: 추억 점수, Time: 0.96 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,15 +1,28 @@
 def solution(name, yearning, photo):
-    # 변수 지정 
-    answer = []
-    dic = {i:j for i, j in zip(name, yearning)}
+    # 변수 정의 
+    dic = {n:y for n, y in zip(name, yearning)}
     
-    # 그리움 점수 합산 
+    # 출력 
+    return [sum(dic[n] for n in p if n in dic) for p in photo]
+
+'''
+
+## 기존 코드 
+
+def solution(name, yearning, photo):
+    # 변수 정의 
+    result = []
+    dic= {n:y for n, y in zip(name, yearning)}
+    
+    # 추억 점수 카운트 
     for p in photo:
         score = 0
         for n in p:
             if n in name:
                 score += dic[n]
-        answer.append(score)
-
+        
+        result.append(score)
+    
     # 출력
-    return answer
+    return result
+'''
