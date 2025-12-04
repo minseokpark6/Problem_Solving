a1aab8d627994f4ec2979ddafe74080b63a170ca
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-12-04 12:36:04 +0900

    [level 2] Title: 할인 행사, Time: 82.00 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -3,12 +3,30 @@ from collections import Counter
 def solution(want, number, discount):
     # 변수 지정
     cnt = 0
-    dic = {w:n for w, n in zip(want, number)}
+    need = {w:n for w, n in zip(want, number)}
+    window = Counter(discount[:10])     # 첫 윈도우 정의
     
-    # 구매하고자 하는 제품과 세일 제품 비교
-    for i in range(len(discount)):
-        if dic == Counter(discount[i:i+10]):
-            cnt += 1
+    # 첫 윈도우 비교 
+    if need == window:
+        cnt += 1
+    
+    # 이후 윈도우와 구매하고자 하는 제품 비교
+    for i in range(10, len(discount)):
+        # 한 칸씩 슬라이싱 
+        out_product = discount[i-10]
+        in_product = discount[i]
+        
+        # 빠지는 제품 카운트 감소
+        window[out_product] -= 1
+        if window[out_product] == 0:
+            del window[out_product]
+        
+        # 새로 추가된 제품 카운트 
+        window[in_product] += 1
+        
+        # 비교
+        if window == need:
+            cnt += 1  
     
     # 출력
     return cnt
@@ -16,6 +34,9 @@ def solution(want, number, discount):
 
 
 '''
+## 개선점 
+(1) for문 내 'Counter(discount[i:i+10])' >> 매번 슬라이싱이 들어가는 것이 비효율적 
+- 슬라이싱 윈도우로 해결 가능
 
 
 ## 기존 통과 코드 
