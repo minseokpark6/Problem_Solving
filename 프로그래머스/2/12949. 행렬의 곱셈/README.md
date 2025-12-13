33c6dfacf180fbaa6c601acec7c1730179741c97
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-12-13 20:20:59 +0900

    [level 2] Title: 행렬의 곱셈, Time: 40.50 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/README.md
+++ 프로그래머스/2/12949. 행렬의 곱셈/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬의 곱셈 - 12949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949) 
 
 ### 성능 요약
 
-메모리: 10.8 MB, 시간: 81.67 ms
+메모리: 10.1 MB, 시간: 40.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 14일 13:23:22
+2025년 12월 12일 20:21:02
 
 ### 문제 설명
 
--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,16 +1,45 @@
 def solution(arr1, arr2):
     # 변수 정의
-    answer = []
-    
+    result = []
+    arr2_col = list(zip(*arr2))    # arr2를 열 기준 리스트로 변환(전치 행렬)
+     
     # 행렬의 곱셈
+    for row in arr1:
+        temp = []
+        for col in arr2_col:
+            temp.append(sum(r*c for r, c in zip(row, col)))
+        # 출력
+        result.append(temp)
+
+    # 출력
+    return result
+
+
+
+'''
+## 개선점
+(1) 전치행렬을 활용하여 for문을 3개에서 2개로 축소 
+-> 시간 복잡도 개선 
+(2) 복잡한 인덱스 접근을 최소화
+
+
+## 기존 통과 코드
+def solution(arr1, arr2):
+    # 변수 정의 
+    result = []
+    
+    # 행렬의 곱셈 
     for i in range(len(arr1)):
         temp = []
         for j in range(len(arr2[0])):
-            a = 0
-            for k in range(len(arr2)):        # 행렬의 곱은 arr1의 열의 개수와 arr2의 행의 개수가 같아야 가능
-                a += arr1[i][k] * arr2[k][j]
-            temp.append(a)
-        answer.append(temp)
-
+            n = 0
+            for k in range(len(arr2)):      # 행렬의 곱은 arr1의 열의 개수와 arr2의 행의 개수가 같아야 가능
+                n += arr1[i][k] * arr2[k][j]
+                
+            temp.append(n)
+        result.append(temp)
+    
     # 출력
-    return answer
+    return result
+
+'''
