ac39cf588e1f2bb2f0a985a17c6a4c64c52786dc
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-12-15 21:13:47 +0900

    [level 2] Title: H-Index, Time: 0.21 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,4 +1,14 @@
 def solution(citations):
+    # 정렬 
+    citations.sort(reverse=True)
+
+    # 출력 
+    return max((i for i, c in enumerate(citations, start=1) if c>=i), default=0)
+
+'''
+## 개선 코드 1
+
+def solution(citations):
     # 정렬
     citations.sort(reverse=True)
     
@@ -10,7 +20,7 @@ def solution(citations):
     # 그 이외의 경우
     return len(citations)
 
-'''
+
 ## 개선점 
 (1) 정렬과 인덱스를 이용하여 시간 복잡도 개선
 - 최대 O(n²) -> O(n log n)
