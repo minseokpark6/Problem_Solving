a63abc2bff949dfbda13fecc713336fd9f435658
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-12-20 14:49:43 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.08 ms
+메모리: 9.29 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 29일 11:38:57
+2025년 12월 20일 14:49:37
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,29 +1,22 @@
 def solution(progresses, speeds):
-    # 변수 지정
-    answer = []
-    days = []
-    
-    # 각 작업 별 완료 일 설정
-    for idx, pro in enumerate(progresses):
-        d = 0
-        while pro < 100:
-            pro += speeds[idx]
-            d += 1
-        days.append(d)
-    
+    # 각 작업 별 완료일 정의 
+    dates = [(100-p)//s if (100-p)%s == 0 else (100-p)//s + 1 for p, s in zip(progresses, speeds)]
+
     # 배포일 별 기능 개수 카운트
-    release = days[0]
-    count = 1
-    
-    for day in days[1:]:
-        if release >= day:
-            count += 1
+    result, stk = [], []
+
+    for d in dates:
+        if not stk:
+            stk.append(d)
+            continue
+        
+        if stk[0] >= d:
+            stk.append(d)
         else:
-            answer.append(count)
-            release = day
-            count = 1
-            
-    answer.append(count)
-    
-    # 출력 
-    return answer
+            result.append(len(stk))
+            stk = []
+            stk.append(d)
+
+    result.append(len(stk))
+
+    return result
