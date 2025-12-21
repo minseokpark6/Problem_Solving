9daf2c79fe84a6acfe92657fd71890d8872b2f39
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-12-21 23:19:36 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,5 +1,30 @@
 def solution(progresses, speeds):
     # 각 작업 별 완료일 정의 
+    days = [(100-p)//s if (100-p)%s == 0 else (100-p)//s + 1 for p, s in zip(progresses, speeds)]
+
+    # 배포일 별 기능 개수 카운트
+    result = []
+    current_day = days[0]
+    cnt = 1
+
+    for d in days[1:]:
+        if d <= current_day:
+            cnt += 1        
+        else:
+            result.append(cnt)
+            current_day = d
+            cnt = 1
+
+    result.append(cnt)
+
+    # 출력
+    return result
+
+'''
+
+## 기존 코드 
+def solution(progresses, speeds):
+    # 각 작업 별 완료일 정의 
     dates = [(100-p)//s if (100-p)%s == 0 else (100-p)//s + 1 for p, s in zip(progresses, speeds)]
 
     # 배포일 별 기능 개수 카운트
@@ -20,3 +45,5 @@ def solution(progresses, speeds):
     result.append(len(stk))
 
     return result
+
+'''
