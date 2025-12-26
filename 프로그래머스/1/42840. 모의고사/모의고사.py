4d4ceeb0070394beb2e5b18dc6ce05edc917c5bd
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-12-26 22:37:11 +0900

    [level 1] Title: 모의고사, Time: 5.01 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,5 +1,32 @@
 def solution(answers):
     # 변수 지정
+    patterns = [
+        [1, 2, 3, 4, 5],
+        [2,1,2,3,2,4,2,5],
+        [3,3,1,1,2,2,4,4,5,5]
+    ]
+
+    result = [0, 0, 0]
+    
+    # 정답 개수 카운트 
+    for idx, answer in enumerate(answers):
+        for i, pattern in enumerate(patterns):
+            if answer == pattern[idx % len(pattern)]:
+                result[i] += 1
+    
+    # 출력
+    max_score = max(result)
+    return [i+1 for i, s in enumerate(result) if s == max_score]
+
+'''
+# 개선점
+(1) 기존 통과 코드에서 불필요한 루프를 -> pattern[idx % len(pattern)]로 대체 
+
+
+## 기존 통과 코드 
+
+def solution(answers):
+    # 변수 지정
     circle = (len(answers)//5)+1
     supo_1 = [1,2,3,4,5] * circle
     supo_2 = [2,1,2,3,2,4,2,5] * circle
@@ -19,3 +46,5 @@ def solution(answers):
     
     # 출력
     return [i+1 for i, c in enumerate(result) if c == max(result)]
+    
+'''
