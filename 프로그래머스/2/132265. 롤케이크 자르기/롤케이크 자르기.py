c891f24669c26f819c5d5bb8d841ab3651b2c625
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2025-12-30 15:59:57 +0900

    [level 2] Title: 롤케이크 자르기, Time: 771.48 ms, Memory: 49.6 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,27 +1,26 @@
-from collections import Counter
+from collections import Counter 
+
 def solution(topping):
-    # 변수 정의
-    answer = 0
+    # 변수 정의 
+    cnt = 0
     
-    # 철수와 동생이 케익을 나누는 변수 정의
-    cheol = dict(Counter(topping))   # 우선 철수가 케익을 모두 먹는다고 가정
-    bro = set()
+    # 우선 한 쪽이 모두 먹는다고 가정 
+    older_bro = dict(Counter(topping))
+    younger_bro = set()
     
     # 토핑의 개수가 일치하는 경우 카운트 
-    for top in topping:
-        # 철수가 먹은 토핑에서 1개 개수 제거
-        cheol[top] -= 1
+    for t in topping:
+        # 철수의 롤케이크에서 토핑 제거 
+        older_bro[t] -= 1
+        if older_bro[t] == 0:
+            del older_bro[t]
         
-        # 개수가 0인 경우 cheol에서 삭제
-        if cheol[top] == 0:
-            del cheol[top]
+        # 동생의 롤케이크에 토핑 추가 
+        younger_bro.add(t)
         
-        # bro에 토핑 추가
-        bro.add(top)
-        
-        # 길이가 같은 경우 카운트
-        if len(bro) == len(cheol):
-            answer += 1
+        # 토핑의 개수가 같은 경우 카운트 
+        if len(older_bro) == len(younger_bro):
+            cnt += 1
 
-    # 출력
-    return answer
+    # 출력        
+    return cnt
