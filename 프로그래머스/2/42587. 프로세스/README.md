3b6ae811ae36bb3a1a23e9eb296f3d7fbd1d20c3
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-01-08 20:47:17 +0900

    [level 2] Title: 프로세스, Time: 0.18 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.21 ms
+메모리: 9.28 MB, 시간: 0.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 18일 21:24:17
+2026년 01월 08일 20:47:12
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,17 +1,46 @@
+from collections import deque
+
 def solution(priorities, location):
-    # 변수 설정 
-    answer = 0 
-    # 우선 순위와 location 설정
+    # 변수 정의
+    q = deque([(i, p) for i, p in enumerate(priorities)])
+    sorted_pr = sorted(priorities, reverse=True)
+    
+    answer = 0
+    
+    # 실행 순서 추적
+    while q:
+        idx, priority = q.popleft()
+        
+        if priority == sorted_pr[0]:
+            answer += 1
+            sorted_pr.pop(0)
+
+            if idx == location:
+                return answer
+        
+        else:
+            q.append((idx, priority))
+'''
+## 개선점
+(1) 시간 복잡도 개선
+- deque 메소드를 활용하여 remove or pop 최소 사용
+
+
+## 이전 통과 코드 
+
+def solution(priorities, location):
+    # 변수 설정
+    order = 0 
+    # 우선 순위와 로케이션 설정 
     pr = [[idx, i] for idx, i in enumerate(priorities)]
     # priorities 내림차순 정렬 
     sorted_pr = sorted(priorities, reverse=True)
     
-    
-    # location의 실행 순서 
+    # 실행순서 찾기 
     while pr:
         temp = pr[0]
-        pr.remove(temp)
-        # 가장 큰 수 설정
+        pr.pop(0)
+        # 가장 우선순위가 높은 수 설정 
         first = sorted_pr[0]
         
         if temp[1] != first:
@@ -20,12 +49,13 @@ def solution(priorities, location):
         elif temp[1] == first:
             # location 확인
             if temp[0] == location:
-                answer += 1
+                order += 1
                 break
             else:
-                sorted_pr.remove(first)
-                answer += 1
-            
-
+                sorted_pr.pop(0)
+                order += 1
+    
     # 출력
-    return answer
+    return order
+            
+'''
