e14ceb0dd613a3e1fe07253490c1d50bf7a6a238
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-01-16 19:05:55 +0900

    [level 1] Title: 소수 만들기, Time: 14.87 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,3 +1,38 @@
+from itertools import combinations
+import math 
+
+def is_prime(n):
+    if n < 2:
+        return False 
+    
+    for i in range(2, int(math.sqrt(n))+1):
+        if n % i == 0:
+            return False
+    
+    return True
+    
+
+def solution(nums):
+    # 변수 정의
+    cnt = 0
+    
+    # 소수 개수 카운트 
+    for a, b, c in combinations(nums, 3):
+        if is_prime(a+b+c):
+            cnt += 1
+    
+    # 출력
+    return cnt
+
+'''
+## 개선점
+(1) 소수 판별 속도 개선
+- 불필요한 숫자까지 모두 돌아가지 않게 소수 판별 범위 축소
+(2) 불필요한 메모리 사용 개선
+- sums 리스트 삭제 
+
+
+## 이전 코드
 from itertools import combinations as comb
 
 def solution(nums):
@@ -15,9 +50,9 @@ def solution(nums):
         
 
     
-"""
+
 # combinations 
 - itertools 모듈에 포함되어 있는 함수
 - 주어진 iterable에서 순서를 고려하지 않고 특정 길이의 모든 조합을 생성하는 데 사용
 - 조합을 나타내는 튜플을 반환
-"""
+'''
