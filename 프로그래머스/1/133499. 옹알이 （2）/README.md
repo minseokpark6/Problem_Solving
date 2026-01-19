535a11d52bb9dd71eb1371072b150f3d2c9dd3a4
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-01-19 12:40:48 +0900

    [level 1] Title: 옹알이 (2), Time: 0.08 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/README.md
+++ 프로그래머스/1/133499. 옹알이 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 1] 옹알이 (2) - 133499 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.21 ms
+메모리: 9.15 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 09일 11:21:56
+2026년 01월 19일 12:40:42
 
 ### 문제 설명
 
--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,3 +1,42 @@
+
+## 기존 replace 방식 유지 개선 코드 
+
+def solution(babbling):
+    # 변수 정의 
+    words = ["aya", "ye", "woo", "ma"]
+    cant = ["ayaaya", "yeye", "woowoo", "mama"]
+    cnt = 0
+    
+    # 발음 가능한 단어 카운트 
+    for b in babbling:
+        word = b
+        
+        for c in cant:
+            if c in word:
+                word = 'x'
+                break
+        
+        if word == 'x':
+            continue 
+        
+        for w in words:
+            word = word.replace(w, " ")
+        
+        if word.strip() == "":
+            cnt += 1
+        
+    
+    # 출력
+    return cnt 
+
+'''
+## 개선점 
+(1) 입력 리스트의 직접 변경 X
+(2) 불필요한 replace 최소화 
+
+
+## 이전 코드
+
 def solution(babbling):
     # 변수 설정
     words = ["aya", "ye", "woo", "ma"]
@@ -19,4 +58,4 @@ def solution(babbling):
     # 출력
     return babbling.count("")
 
-
+'''
