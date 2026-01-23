414adeb580529b5bf78f051ffb724f6ad31d0040
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-01-23 13:28:59 +0900

    [level 1] Title: 옹알이 (2), Time: 0.22 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,4 +1,39 @@
+# 순차 탐색 버전
+def solution(babbling):
+    # 변수 정의
+    words = ["aya", "ye", "woo", "ma"]
+    cnt = 0
+    
+    # 발음 가능한 단어 개수 카운트 
+    for b in babbling:
+        # 단어 비교를 위한 변수 정의
+        prev = ""       # 중복 단어 방지 
+        i = 0           # 단어의 index
+        valid = True    # 발음 가능한 단어인지 여부 
+        
+        # 주어진 단어가 발음 가능한 단어인지 여부 
+        while i < len(b):
+            matched = False
+            
+            for w in words:
+                if b.startswith(w, i) and w != prev:
+                    matched = True 
+                    prev = w 
+                    i += len(w)
+                    break
+            
+            if not matched:
+                valid = False 
+                break 
+        
+        if valid:
+            cnt += 1
+    
+    # 출력 
+    return cnt
 
+
+'''
 ## 기존 replace 방식 유지 개선 코드 
 
 def solution(babbling):
@@ -29,7 +64,6 @@ def solution(babbling):
     # 출력
     return cnt 
 
-'''
 ## 개선점 
 (1) 입력 리스트의 직접 변경 X
 (2) 불필요한 replace 최소화 
