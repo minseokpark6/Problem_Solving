c9f5d5e8779cc3a760f2023f089234bf2fd55a1c
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-01-26 23:43:40 +0900

    [level 2] Title: 모음 사전, Time: 1.12 ms, Memory: 9.44 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,12 +1,37 @@
-from itertools import product
+from itertools import product 
 
 def solution(word):
-    # 다섯가지 모음으로 만들 수 있는 길이 5 이하의 모든 단어 리스트
-    letters = ['A', 'E', 'I', 'O', 'U']
-    result = [''.join(comb) for length in range(1, 6) for comb in product(letters, repeat=length)]
+    # 알파벳 모음으로 만들 수 있는 단어 리스트 생성 
+    letters = ['A', 'E', 'I', 'O', 'U'] 
+    word_list = sorted(["".join(c) for length in range(1, 6) for c in product(letters, repeat=length)])
     
-    # 해당 단어의 인덱스 찾기
-    idx = sorted(result).index(word)
-    
-    # 출력
-    return idx+1
+    # 출력 
+    return word_list.index(word) + 1
+
+'''
+
+
+## 순열 / 조합
+
+(1) 순열 permutations 
+- 반복 허용 X 
+- 순서 고려 O 
+- (1, 2) (1, 3) (1, 4) (2, 1) (2, 3) (2, 4) (3, 1) (3, 2) (3, 4) (4, 1) (4, 2) (4, 3)
+
+(2) 조합 combinations 
+- 반복 허용 X 
+- 순서 고려 X 
+- (1, 2) (1, 3) (1, 4) (2, 3) (2, 4) (3, 4) 
+
+(3) 중복 순열 product 
+- 반복 허용 O
+- 순서 고려 O 
+- (1, 1) (1, 2) (1, 3) (1, 4) (2, 1) (2, 2) (2, 3) (2, 4) (3, 1) (3, 2) (3, 3) (3, 4) (4, 1) (4, 2) (4, 3) (4, 4)
+
+(4) 중복 조합 combinations_with)_replacement
+- 반복 허용 O 
+- 순서 고려 X 
+- (1, 1) (1, 2) (1, 3) (1, 4) (2, 2) (2, 3) (2, 4) (3, 3) (3, 4) (4, 4) 
+
+
+'''
