6e28bf202cb80deb044a964ef1e95c92b2e29c8d
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-02-22 19:29:55 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 74.01 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
@@ -1,4 +1,14 @@
 def solution(n):
+    # 나머지가 1이 되는 가장 작은 수 찾기
+    return next(i for i in range(2, n) if n%i==1)
+
+'''
+## next 
+- 조건을 만족하는 첫 번째 값 반환
+
+
+## 이전 코드
+def solution(n):
     # 변수 설정
     num = 2
     # 나머지가 1이 되는 가장 작은 수 찾기
@@ -10,3 +20,5 @@ def solution(n):
 
     # 출력
     return answer
+'''
+        
