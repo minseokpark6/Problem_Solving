78da2f7277cf34604a983821fc263e69d4792e79
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-03 06:25:57 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -1,4 +1,12 @@
 def solution(n):
+    return [i for i in range(n+1) if i%2==1]
+
+
+
+''' 
+## 이전 통과 코드
+
+def solution(n):
     answer = []
     if n % 2 == 0:
         for i in range(1, n, 2):
@@ -7,3 +15,5 @@ def solution(n):
         for i in range(1, n+1, 2):
             answer.append(i)
     return answer
+
+'''
