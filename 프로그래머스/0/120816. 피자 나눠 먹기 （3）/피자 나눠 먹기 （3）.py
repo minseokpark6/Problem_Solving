aa2876b6968bd5ab533fc700d57dd9d7ecb61fdd
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-04 06:12:53 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,6 +1,14 @@
 def solution(slice, n):
+    result = n//slice
+    return result if n%slice == 0 else result+1
+    
+    
+    
+'''
+def solution(slice, n):
     answer = n // slice
     if n % slice == 0:
         return answer 
     else:
         return answer + 1
+'''
