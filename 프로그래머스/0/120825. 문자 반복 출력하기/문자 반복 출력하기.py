95f8130fbc9433a90ee2768df1df04db35cacee1
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-04 06:24:51 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.00 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
@@ -1,5 +1,15 @@
 def solution(my_string, n):
+    result = ''
+    for s in my_string:
+        result += s*n
+    return result
+
+
+'''
+def solution(my_string, n):
     answer = ''
     for idx in range(len(my_string)):
         answer += (my_string[idx] * n)
     return answer
+
+'''
