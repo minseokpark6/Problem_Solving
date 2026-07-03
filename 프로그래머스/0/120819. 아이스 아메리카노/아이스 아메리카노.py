cf52a74ec2b7ab5c28b7954f3042b507b616be05
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-04 06:18:13 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,7 +1,13 @@
 def solution(money):
+    return [money // 5500, money%5500]
+
+'''
+
+def solution(money):
     answer = []
     n = money // 5500
     answer.append(n)
     r = money - (5500*n)
     answer.append(r)
     return answer
+'''
