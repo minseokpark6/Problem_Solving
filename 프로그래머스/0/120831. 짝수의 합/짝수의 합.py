744b6d439d30321726875ebd59ec4667fe5c9fbd
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-06 11:36:24 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
@@ -1,6 +1,13 @@
 def solution(n):
+    return sum(i for i in range(n+1) if i%2==0)
+
+
+'''
+def solution(n):
     arr1 = range(2, n+1, 2)
     sum = 0 
     for i in arr1: 
         sum += i
     return sum
+
+'''
