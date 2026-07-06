0526320aa0b784f086fe8d2768e9836849518326
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-06 11:29:01 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.py
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -1,4 +1,18 @@
 def solution(angle):
+    if angle < 90:
+        return 1
+    elif angle == 90:
+        return 2
+    elif angle < 180:
+        return 3
+    else:
+        return 4
+    
+    
+
+    
+'''
+def solution(angle):
     if angle < 90 : 
         return 1
     elif angle == 90 :
@@ -7,3 +21,4 @@ def solution(angle):
         return 3 
     else : 
         return 4
+'''
