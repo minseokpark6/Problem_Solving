123cd2ce01ef0bb622e14e32546b0e14c5f39afd
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-06 11:31:03 +0900

    [level 0] Title: 양꼬치, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.py
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.py
@@ -1,3 +1,11 @@
 def solution(n, k):
+    l, b = 12000, 2000
+    return n*l + k*b - (n//10 * b)
+
+
+'''
+def solution(n, k):
     answer = n*12000 + k*2000 - (n // 10) * 2000
     return answer
+
+'''
