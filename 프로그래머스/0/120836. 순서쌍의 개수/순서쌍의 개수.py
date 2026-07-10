b9957becc297be9990d4ddac2ef5f09fb3244bc9
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-10 15:58:28 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.11 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,6 +1,23 @@
+import math
+
+def solution(n):
+    # 변수 정의 
+    cnt = 0
+    root = math.isqrt(n)
+    # 순서쌍 개수 => 약수의 개수 카운트 
+    for i in range(1, root+1):
+        if i ** 2 ==n:
+            cnt += 1
+        elif n % i ==0:
+            cnt += 2
+    # 출력
+    return cnt
+
+'''
 def solution(n):
     answer = 0
     for i in range(1, n+1):
         if n % i == 0:
             answer += 1
     return answer
+'''
