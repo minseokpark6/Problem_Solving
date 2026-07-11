90a917d3148a2fda20fe6c1259d186d77a5ad5ca
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-11 10:43:57 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
@@ -1,5 +1,10 @@
 def solution(strlist):
+    return [len(s) for s in strlist]
+
+'''
+def solution(strlist):
     answer = []
     for str in strlist:
         answer.append(len(str))
     return answer
+'''
