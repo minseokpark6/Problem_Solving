65b8503c0befcdb97be26b9081977ab8d89b4721
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-11 10:42:06 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -1,4 +1,11 @@
 def solution(my_string):
+    # 문자열에서 숫자 골라내기
+    num_list = [int(ch) for ch in my_string if ch.isdigit()]
+    # 출력
+    return sum(num_list)
+
+'''
+def solution(my_string):
     # 초기화
     answer = 0
     # my_string에서 숫자만 추출(숫자도 타입은 str)
@@ -7,3 +14,4 @@ def solution(my_string):
         num = int(num)
         answer += num
     return answer
+'''
