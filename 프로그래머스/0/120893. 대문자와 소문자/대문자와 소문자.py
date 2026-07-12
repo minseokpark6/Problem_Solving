4ff0d442a01c8547383cd8e2b931b770b914430b
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-12 14:42:07 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.10 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,3 +1,13 @@
 def solution(my_string):
+    return "".join(
+        c.upper() if c.islower() else c.lower() 
+        for c in my_string
+    )
+
+
+
+'''
+def solution(my_string):
     answer = my_string.swapcase()
     return answer
+'''
