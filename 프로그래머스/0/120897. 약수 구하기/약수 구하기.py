9f633a243d42cbc69d1815cc4a32d90f4ae48f18
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-14 11:18:06 +0900

    [level 0] Title: 약수 구하기, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -1,6 +1,31 @@
+import math
+
+def solution(n):
+    # 변수 정의 
+    root = math.isqrt(n)
+    small, large = [], []
+
+    # 약수 구하기 
+    for i in range(1, root+1):
+        if n%i == 0:
+            small.append(i)
+            if i != n//i:
+                large.append(n//i)
+
+    # 출력
+    return small + large[::-1]
+
+
+'''
+## 개선점
+- 시간 복잡도 개선
+    - 루트를 활용하여 for 반복문 복잡도를 낮췄고, 
+    - 리스트 두개를 활용하여 정렬 시간 복잡도를 낮춤
+
 def solution(n):
     answer = []
     for i in range(1, n+1):
         if n % i == 0 :
             answer.append(i)
     return answer
+'''
