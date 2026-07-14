5f29ae8df1611519e70a21c29fa4239e2a582b71
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-14 11:29:38 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -1,4 +1,16 @@
 def solution(array):
+    # 인덱스 값을 포함하여 딕셔너리로 변환
+    dic = {n:idx for idx, n in enumerate(array)}
+    
+    # 출력
+    return list(sorted(dic.items(), reverse=True)[0])
+
+
+'''
+## 개선점 
+- 딕셔너리를 활용한 시간 복잡도 개선 
+
+def solution(array):
     max_num = array[0]
     answer = []
     
@@ -12,3 +24,4 @@ def solution(array):
     answer.append(idx)
     
     return answer
+'''
