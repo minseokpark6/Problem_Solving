749363bf4b1b279fbac318622ef6e21f1d94c3df
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-15 16:33:39 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
@@ -1,7 +1,12 @@
 def solution(str1, str2):
+    return 1 if str2 in str1 else 2
+
+'''
+def solution(str1, str2):
     result = str2 in str1
     if result == True:
         answer = 1
     else:
         answer = 2
     return answer
+'''
