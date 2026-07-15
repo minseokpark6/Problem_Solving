1da08a9d5509f24652ad601af60c44ecf1adf736
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-15 16:22:10 +0900

    [level 0] Title: 배열의 유사도, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,7 +1,18 @@
 def solution(s1, s2):
+    return len(set(s1) & set(s2))
+
+
+'''
+## 개선점 
+(1) 시간 복잡도 개선 
+- 이중 for 문은 모든 리스트 원소를 다 돌아야함으로 비효율적
+- 집합(set)을 이용해 리스트의 탐색 시간인 O(n)을 O(1)로 단축
+
+def solution(s1, s2):
     answer = 0
     for i in s1:
         for j in s2:
             if i == j:
                 answer += 1
     return answer
+'''
