c1d05fca2e20b168bd2f1eb2563502b5ebd451aa
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-15 16:25:56 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.00 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
@@ -1,3 +1,9 @@
 def solution(n, numlist):
+    return [num for num in numlist if num%n==0]
+
+
+'''
+def solution(n, numlist):
     answer = [num for num in numlist if num % n == 0]
     return answer
+'''
