ce1e41cbad049d58a88a714b765722d0d13805a6
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-15 16:30:45 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.py
@@ -1,5 +1,11 @@
 def solution(n):
+    return sum(map(int, str(n)))
+
+'''
+
+def solution(n):
     answer = 0
     for i in range(len(str(n))):
         answer += int(str(n)[i])
     return answer
+'''
