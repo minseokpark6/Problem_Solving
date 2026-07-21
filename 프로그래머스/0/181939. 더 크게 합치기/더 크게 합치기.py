f3e6df27255cdaf0eb6b6b818122f7ead06ca41a
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-21 20:44:49 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,4 +1,12 @@
 def solution(a, b):
+    # 변수 정의
+    r1, r2 = int(str(a)+str(b)), int(str(b)+str(a))
+    # 츨력
+    return r1 if r1 >= r2 else r2
+    
+
+'''
+def solution(a, b):
     result_1 = int(str(a) + str(b))
     result_2 = int(str(b) + str(a))
     if result_1 >= result_2 : 
@@ -6,3 +14,4 @@ def solution(a, b):
     else:
         answer = result_2
     return answer
+'''
