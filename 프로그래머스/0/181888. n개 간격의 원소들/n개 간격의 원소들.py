954d4e73576df61222894f393be0e7ef3ee67991
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-21 20:48:08 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.py
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.py
@@ -1,5 +1,11 @@
 def solution(num_list, n):
+    return [num_list[i] for i in range(0, len(num_list), n)]
+
+
+'''
+def solution(num_list, n):
     answer = []
     for i in range(0, len(num_list), n):
         answer.append(num_list[i])
     return answer
+'''
