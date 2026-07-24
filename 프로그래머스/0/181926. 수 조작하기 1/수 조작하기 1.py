12c895e9dc183d27588be5e429bd6a8c5318c0eb
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-24 09:38:56 +0900

    [level 0] Title: 수 조작하기 1, Time: 3.00 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,4 +1,21 @@
 def solution(n, control):
+    # 동작 값 정의 
+    move = {
+        "w" : 1,
+        "s" : -1,
+        "d" : 10,
+        "a" : -10
+    }
+
+    # control 순서에 따라 변경
+    for c in control:
+        n += move[c]
+    
+    # 출력
+    return n
+
+'''
+def solution(n, control):
     for s in control:
         if s == 'w':
             n += 1
@@ -10,3 +27,4 @@ def solution(n, control):
             n -= 10
 
     return n
+'''
