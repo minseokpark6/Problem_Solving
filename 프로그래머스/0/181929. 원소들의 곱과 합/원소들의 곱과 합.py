d8e4db734594f254c760effe61f913a7a1ccecd9
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-27 10:29:32 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,4 +1,18 @@
 def solution(num_list):
+    # 변수정의 
+    m = 1
+    s = sum(num_list) ** 2 
+    
+    # 모든 원소의 곱 구하기
+    for i in num_list:
+        m *= i
+    
+    # 출력
+    return 1 if m<s else 0
+
+
+'''
+def solution(num_list):
     sum = 0
     mul = 1
     # num_list 내의 숫자의 합과 곱 구하기
@@ -14,3 +28,4 @@ def solution(num_list):
         return 1
     elif sum < mul :
         return 0
+'''
