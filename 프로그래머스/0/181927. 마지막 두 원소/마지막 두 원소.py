889070d755ddbeaa500b596cadc51f63410645d6
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-28 19:48:04 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,7 +1,20 @@
 def solution(num_list):
+    # 변수 정의 
+    a, b = num_list[-1], num_list[-2]
+    # num_list 원소 추가 
+    if a > b:
+        num_list.append(a-b)
+    else:
+        num_list.append(a*2)
+    # 출력
+    return num_list
+
+'''
+def solution(num_list):
     if num_list[-1] > num_list[-2]:
         a = num_list[-1] - num_list[-2]
     else:
         a = num_list[-1] * 2
     num_list.append(a)
     return num_list
+'''
