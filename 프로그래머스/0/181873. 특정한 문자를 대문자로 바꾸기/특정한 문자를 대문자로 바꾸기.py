a1f2401c862dc843fb71d714443fd6ca52854762
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-28 20:03:58 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.03 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.py
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.py
@@ -1,7 +1,12 @@
 def solution(my_string, alp):
+    return my_string.replace(alp, alp.upper())
+
+'''
+def solution(my_string, alp):
     answer = ""
     for i in my_string:
         if i == alp:
             i = i.upper()
         answer += i 
     return answer
+'''
