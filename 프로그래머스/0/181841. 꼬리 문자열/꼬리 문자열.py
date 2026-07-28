a84ee26033f05fddd7fb2901377b95a8792ef8de
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-28 19:50:45 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
@@ -1,4 +1,8 @@
 def solution(str_list, ex):
+    return "".join(s for s in str_list if ex not in s)
+
+'''
+def solution(str_list, ex):
     result = []
     answer = []
     for str in str_list:
@@ -6,3 +10,4 @@ def solution(str_list, ex):
             result.append(str)
     answer = "".join(result)
     return answer
+'''
