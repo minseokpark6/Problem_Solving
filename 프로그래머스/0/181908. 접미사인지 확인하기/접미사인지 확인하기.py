410f5daaba21b5c3613ae070c788cffc65febad5
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-30 20:00:59 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,7 +1,14 @@
 def solution(my_string, is_suffix):
+    # 문자열에 대한 접미사 배열 생성
+    suffix = [my_string[i:] for i in range(len(my_string))]
+    # 출력
+    return 1 if is_suffix in suffix else 0
+    
+'''
+def solution(my_string, is_suffix):
     suffix = [my_string[i:] for i in range(len(my_string))]
     if is_suffix in suffix:
         return 1
     else:
         return 0
-    return answer
+'''
