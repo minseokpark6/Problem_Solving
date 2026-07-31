9991d2b7a3908f606b9bff52a50c871ff1c95f25
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-07-31 12:43:18 +0900

    [level 0] Title: 주사위의 개수, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.py
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.py
@@ -1,4 +1,18 @@
 def solution(box, n):
+    # 변수정의
+    cnt = 0
+    # 주사위 최대 개수 구하기
+    for l in box:
+        if cnt == 0:
+            cnt = (l//n)
+        else:
+            cnt *= (l//n)
+    # 출력
+    return cnt
+
+
+'''
+def solution(box, n):
     answer = 0
     a = box[0] // n
     answer += a
@@ -10,3 +24,36 @@ def solution(box, n):
     answer *= c
 
     return answer
+
+
+'''
+def solution(box, n):
+    answer = 0
+    a = box[0] // n
+    answer += a
+
+    b = box[1] // n
+    answer *= b
+
+    c = box[2] // n
+    answer *= c
+
+    return answer
+'''
+'''
+
+
+'''
+def solution(box, n):
+    answer = 0
+    a = box[0] // n
+    answer += a
+
+    b = box[1] // n
+    answer *= b
+
+    c = box[2] // n
+    answer *= c
+
+    return answer
+'''
