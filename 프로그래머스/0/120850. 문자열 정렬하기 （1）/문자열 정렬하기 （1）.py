2a6b1a071f0ab12be4769d2f6b1918fc7e964578
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-01 14:55:16 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,3 +1,9 @@
 def solution(my_string):
+    return sorted(int(n) for n in my_string if n.isdigit())
+
+'''
+def solution(my_string):
     answer = sorted([int(num) for num in my_string if num.isdigit()])
     return answer
+
+'''
