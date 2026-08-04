742771b9658b302a933cafd94b8bd3448b4ef507
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-04 12:45:43 +0900

    [level 0] Title: 0 떼기, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.py
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.py
@@ -1,7 +1,18 @@
 def solution(n_str):
+    # 0으로 시작하지 않는 인덱스 찾기 
+    for i, n in enumerate(n_str):
+        if n != "0":
+            idx = i
+            break
+    # 출력
+    return n_str[idx:]
+
+
+'''
+def solution(n_str):
     result = [i for i in n_str]
     while result[0] == "0":
         del result[0]
     answer = "".join(result)
     return answer
-        
+'''
