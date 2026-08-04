dd10f0a3661facfb43ea39b289033ad6ef8b49d0
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-04 12:33:29 +0900

    [level 0] Title: 숫자 찾기, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
@@ -1,7 +1,15 @@
 def solution(num, k):
+    for idx, n in enumerate(str(num)):
+        if str(k) == n:
+            return idx + 1
+    return -1
+
+'''
+def solution(num, k):
     str_num = str(num)
     if str(k) in str_num:
         idx = str_num.find(str(k)) + 1
         return idx
     else : 
         return -1
+'''
