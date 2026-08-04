e647d63c3c2a6000e5fb9d1521d720ca3569c62d
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-04 12:35:24 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
@@ -1,4 +1,9 @@
 def solution(my_string):
+    return "".join(sorted(my_string.lower()))
+
+'''
+def solution(my_string):
     answer = sorted(list(my_string.lower()))
     answer = ''.join(answer)
     return answer
+'''
