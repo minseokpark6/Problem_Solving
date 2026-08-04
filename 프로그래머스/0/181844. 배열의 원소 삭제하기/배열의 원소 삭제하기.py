ac04195020883f0a93cbd63705afa8de2d2e5eba
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-04 12:41:12 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.06 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -1,13 +1,17 @@
 def solution(arr, delete_list):
+    return [n for n in arr if n not in delete_list]
+
+
+
+'''
+## 이전 통과 코드 
+def solution(arr, delete_list):
     answer = []
     for num in arr:
         if num not in delete_list:
             answer.append(num)
     return answer
 
-
-
-'''
 # for문 내부에서 for문을 돌리는 대상 변수를 조작하고 있는 것이 가장 큰 문제
 
 def solution(arr, delete_list):
