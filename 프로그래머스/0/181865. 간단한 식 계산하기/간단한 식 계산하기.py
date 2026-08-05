d93baa58356eb8eae53bd0704682e4c79f409fa0
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-06 08:35:10 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -1,4 +1,18 @@
 def solution(binomial):
+    # 변수 정의
+    arr = binomial.split(" ")
+    a, op, b = int(arr[0]), arr[1], int(arr[2])
+    
+    # 출력
+    if op == "+":
+        return a+b
+    elif op == '-':
+        return a-b
+    else:
+        return a*b
+    
+'''
+def solution(binomial):
     result = binomial.split(" ")
     a, b = int(result[0]), int(result[2])
     if result[1] == "+":
@@ -8,3 +22,4 @@ def solution(binomial):
     else:
         answer = a*b
     return answer
+'''
