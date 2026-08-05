f7e234004290c38ae1825b998fe0f7ea36e26490
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-06 08:37:47 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 3.53 ms, Memory: 12.5 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
@@ -1,5 +1,10 @@
 def solution(myString):
+    return sorted(s for s in myString.split("x") if s != "")
+
+'''
+def solution(myString):
     answer = []
     result = myString.split("x")
     answer = [s for s in result if s != ""]
     return sorted(answer)
+'''
