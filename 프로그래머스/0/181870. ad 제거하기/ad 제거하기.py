aea8225aa700b40205e2cbb4b4cc2c8a9e37021d
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-06 08:40:25 +0900

    [level 0] Title: ad 제거하기, Time: 0.04 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
@@ -1,6 +1,11 @@
 def solution(strArr):
+    return [s for s in strArr if "ad" not in s]
+
+'''
+def solution(strArr):
     answer = []
     for s in strArr:
         if "ad" not in s:
             answer.append(s)
     return answer
+'''
