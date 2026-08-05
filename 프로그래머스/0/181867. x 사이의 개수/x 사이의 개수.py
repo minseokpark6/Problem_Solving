22186cc51e71c19bbd3605ba5f233ba56075ccc1
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-06 08:38:44 +0900

    [level 0] Title: x 사이의 개수, Time: 1.17 ms, Memory: 12.6 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.py
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.py
@@ -1,4 +1,9 @@
 def solution(myString):
+    return [len(s) for s in myString.split("x")]
+
+'''
+def solution(myString):
     result = myString.split("x")
     answer = [len(str) for str in result]
     return answer
+'''
