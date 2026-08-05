9f5333715b00a488598247359f9999b414194b50
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-05 20:06:23 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,7 +1,15 @@
 def solution(myString, pat):
+    # 변수 정의
+    result = "".join("B" if s == "A" else "A" for s in myString)
+    # 출력
+    return 1 if pat in result else 0
+
+'''
+def solution(myString, pat):
     result = "".join(["B" if s == "A" else "A" for s in myString])
     if pat in result:
         answer = 1
     else:
         answer = 0
     return answer
+'''
