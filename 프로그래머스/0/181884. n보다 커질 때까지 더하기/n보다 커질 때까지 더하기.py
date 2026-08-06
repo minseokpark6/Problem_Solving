57c44229a7d1c186a2f7628c64f9112002cdd284
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-07 07:45:00 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.py
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.py
@@ -1,7 +1,19 @@
 def solution(numbers, n):
+    # 변수 정의
+    result = 0
+    
+    # 출력
+    for i in numbers:
+        result += i
+        if result > n:
+            return result
+
+'''
+def solution(numbers, n):
     answer = 0
     for i in numbers:
         answer += i 
         if answer > n:
             break
     return answer
+'''
