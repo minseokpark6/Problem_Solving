a8d96428119112898344b50cc8c56bb18faed654
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-07 07:33:35 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -1,13 +1,17 @@
 from math import prod
 
 def solution(num_list):
+    return sum(num_list) if len(num_list) >= 11 else prod(num_list)
+
+'''
+# list 누적곱
+math 라이브러리의 prod 사용
+
+from math import prod
+
+def solution(num_list):
     if len(num_list) >= 11:
         return sum(num_list)
     else:
         return prod(num_list)
-
-'''
-# list 누적곱
-
-math 라이브러리의 prod 사용
 '''
