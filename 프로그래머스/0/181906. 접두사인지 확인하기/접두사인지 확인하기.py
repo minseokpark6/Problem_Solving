c9f87c852d174bcec025828c86afa90cc988e3f5
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-10 07:03:14 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,4 +1,11 @@
 def solution(my_string, is_prefix):
+    # 접두사 리스트 정의 
+    arr = [my_string[:idx] for idx in range(1, len(my_string)+1)]
+    # 출력
+    return 1 if is_prefix in arr else 0
+
+'''
+def solution(my_string, is_prefix):
     prefix_list = []
     for i in range(len(my_string)):
         prefix_list.append(my_string[:i])
@@ -6,3 +13,4 @@ def solution(my_string, is_prefix):
         return 1
     else:
         return 0
+'''
