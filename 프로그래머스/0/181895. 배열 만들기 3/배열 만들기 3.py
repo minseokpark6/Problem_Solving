71fc09674f510042d7c5731d6ee318abf8a90ea0
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-09 16:31:16 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.41 ms, Memory: 13.5 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
@@ -1,4 +1,9 @@
 def solution(arr, intervals):
+    return arr[intervals[0][0]:intervals[0][1]+1] + arr[intervals[1][0]:intervals[1][1]+1]
+
+
+'''
+def solution(arr, intervals):
     answer = []
     for i in intervals:
         s, e = i[0], i[1]
@@ -6,3 +11,4 @@ def solution(arr, intervals):
         for n in result:
             answer.append(n)
     return answer
+'''
