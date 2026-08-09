16e0c98abf1a5d209ad86ae12060b252aac3b45a
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-10 07:13:26 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 4.16 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.py
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.py
@@ -1,6 +1,11 @@
 def solution(number):
+    return sum(int(n) for n in number) % 9
+
+'''
+def solution(number):
     sum = 0
     for s in number:
         sum += int(s)
     answer = sum % 9
     return answer
+'''
