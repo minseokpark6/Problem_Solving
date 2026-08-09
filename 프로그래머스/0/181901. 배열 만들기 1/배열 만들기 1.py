58396d87e0b67779cd78db407f545b42a01b542b
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-09 16:37:54 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.06 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
@@ -1,7 +1,12 @@
 def solution(n, k):
+    return list(range(k, n+1, k))
+
+'''
+def solution(n, k):
     answer = []
     arr = list(range(1, n+1))
     for num in arr:
         if num % k == 0:
             answer.append(num)
     return answer
+'''
