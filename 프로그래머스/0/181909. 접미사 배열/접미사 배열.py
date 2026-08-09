7c314b05b2371e0a2bde5c2cda3e0ceb8ee37395
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-10 07:05:30 +0900

    [level 0] Title: 접미사 배열, Time: 0.03 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
@@ -1,4 +1,9 @@
 def solution(my_string):
+    return sorted(my_string[idx:] for idx in range(len(my_string)))
+
+'''
+def solution(my_string):
     result = [my_string[i:] for i in range(len(my_string))]
     answer = sorted(result)
     return answer
+'''
