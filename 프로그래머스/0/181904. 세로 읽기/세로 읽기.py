5c50e56470d858835738823cb202345e0e6451b0
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-09 16:42:52 +0900

    [level 0] Title: 세로 읽기, Time: 0.03 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -1,5 +1,10 @@
 def solution(my_string, m, c):
+    return "".join(my_string[idx] for idx in range(c-1, len(my_string), m))
+
+'''
+def solution(my_string, m, c):
     answer = ''
     for idx in range(c-1, len(my_string), m):
         answer += my_string[idx]
     return answer
+'''
