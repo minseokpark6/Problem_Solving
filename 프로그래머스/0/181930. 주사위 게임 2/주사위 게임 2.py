9884c112fc0e2538c917df199b2a9eab0f940f71
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-11 23:32:57 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,4 +1,18 @@
 def solution(a, b, c):
+    total = a + b + c
+
+    if a == b == c:
+        return total * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
+
+    elif a == b or b == c or a == c:
+        return total * (a**2 + b**2 + c**2)
+
+    else:
+        return total
+         
+        
+'''
+def solution(a, b, c):
     # 세 숫자가 모두 같은 경우
     if a == b and b == c:
         answer = (3 * a) * (3 * (a**2)) * (3 * (a**3))
@@ -16,14 +30,4 @@ def solution(a, b, c):
         answer = a + b + c
     
     return answer
-        
-        
-        
-'''
-# 두 숫자가 같고, 하나의 숫자가 다른 경우
-
-elif a == b or a == c or b == c :
-        answer = (a + b + c) + ((a**2) + (b**2) + (c **2))
-        
->> 이렇게 표현할 경우 세 개의 숫자가 모두 같은 경우까지 포함됨
 '''
