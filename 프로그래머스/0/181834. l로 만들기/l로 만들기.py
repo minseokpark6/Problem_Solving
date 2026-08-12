b34df34f381fda0373859f3f6caad7d522cc3790
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-12 21:47:12 +0900

    [level 0] Title: l로 만들기, Time: 4.74 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -1,4 +1,9 @@
 def solution(myString):
+    return "".join("l" if char < "l" else char for char in myString)
+
+
+'''
+def solution(myString):
     alpha = ["a", "b", "c", "d", "e", "f", 'g', "h", "i", "j", "k"]
     answer = []
     for s in myString:
@@ -8,3 +13,4 @@ def solution(myString):
             answer.append(s)
     answer = "".join(answer)
     return answer
+'''
