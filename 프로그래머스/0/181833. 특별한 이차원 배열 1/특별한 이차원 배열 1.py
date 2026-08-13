0c00731326cf20657d8a475b55d5ed4a8c90e1ad
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-13 10:25:43 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.24 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -1,6 +1,5 @@
 def solution(n):
-    answer = [[1 if i == j else 0 for j in range(n)] for i in range(n)]
-    return answer
+    return [[1 if i == j else 0 for j in range(n)] for i in range(n)]
 
 
 """
