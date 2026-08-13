387c08308cf3f0d81b2be9b1c0d25d7f909c7934
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-13 10:39:30 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.03 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.py
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.py
@@ -1,7 +1,11 @@
 def solution(my_string):
+    return [s for s in my_string.strip().split(" ") if s != ""]
+'''
+def solution(my_string):
     result = my_string.strip(" ").split(" ")
     answer = []
     for s in result:
         if s != '':
             answer.append(s)
     return answer
+'''
