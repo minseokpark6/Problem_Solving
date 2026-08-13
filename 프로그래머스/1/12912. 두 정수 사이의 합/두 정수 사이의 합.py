4df542bbe4380b83fead1a6009b8295c47a47bdb
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-13 10:45:39 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 199.36 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -1,4 +1,8 @@
 def solution(a, b):
+    return sum(range(min(a,b), max(a,b)+1))
+    
+    
+'''
     if a > b:
         result = [n for n in range(b, a+1)]
     elif a < b: 
@@ -7,3 +11,4 @@ def solution(a, b):
         result = [a]
     answer = sum(result)
     return answer
+'''
