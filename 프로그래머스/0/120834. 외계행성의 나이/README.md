43528ad830eb6f085a4d55e24916f0afb4799ce1
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-16 19:20:15 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/README.md
+++ 프로그래머스/0/120834. 외계행성의 나이/README.md
@@ -1,10 +1,10 @@
 # [level 0] 외계행성의 나이 - 120834 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120834#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120834) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.03 ms
+메모리: 11.5 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 14일 14:33:33
+2026년 08월 16일 19:20:09
 
 ### 문제 설명
 
--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -1,7 +1,18 @@
 def solution(age):
+    # 변수 정의 
+    alpha = "abcdefghij"
+    
+    # 나이 변환 출력
+    return "".join(alpha[int(a)] for a in str(age))
+        
+
+
+'''
+def solution(age):
     alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
     answer = []
     for i in list(str(age)):
         answer.append(alpha[int(i)])
     answer = ''.join(answer)
     return answer
+'''
