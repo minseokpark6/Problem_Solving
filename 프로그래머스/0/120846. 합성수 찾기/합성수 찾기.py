f4150195d04a3bdd88659800759353c30e0424d6
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-19 15:05:44 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,3 +1,24 @@
+import math 
+def solution(n):
+    if n < 2:
+        return 0
+    
+    # 에라토스테네스의 체 변수 정의
+    is_prime = [True]*(n+1)
+    is_prime[0] = is_prime[1] = False
+    
+    # 소수 찾기 
+    for i in range(2, math.isqrt(n)+1):
+        if is_prime[i]:
+            for j in range(i*i, n+1, i):
+                is_prime[j] = False
+
+    # 출력
+    return sum(not x for x in is_prime[2:])
+    
+    
+    
+'''
 def solution(n):
     answer = []
     temp = []
@@ -11,3 +32,4 @@ def solution(n):
         temp = []
     
     return len(answer)
+'''
