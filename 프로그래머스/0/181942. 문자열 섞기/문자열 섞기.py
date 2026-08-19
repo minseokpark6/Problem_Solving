00dcc532190323fad5001c33146aeeb56374d26d
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-19 17:23:46 +0900

    [level 0] Title: 문자열 섞기, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
@@ -1,4 +1,8 @@
 def solution(str1, str2):
+    return "".join(i+j for i, j in zip(str1, str2))
+
+'''
+def solution(str1, str2):
     result = []
     for i in range(len(str1)):
         a = str1[i]
@@ -7,3 +11,4 @@ def solution(str1, str2):
         result.append(b)
     answer = ''.join(result)
     return answer
+'''
