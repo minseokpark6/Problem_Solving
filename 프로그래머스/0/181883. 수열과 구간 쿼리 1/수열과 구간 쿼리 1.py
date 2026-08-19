c6874a079e0dc654f4cb3d3ba24614c6fa91add9
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-19 17:11:18 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 5.71 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
@@ -1,6 +1,18 @@
 def solution(arr, queries):
+    # 구간 쿼리 처리 
+    for q in queries:
+        for i in range(q[0], q[1]+1):
+            arr[i] += 1
+    
+    # 출력
+    return arr
+
+'''
+def solution(arr, queries):
     for q in queries:
         s, e = q[0], q[1]
         for i in range(s, e+1):
             arr[i] += 1
     return arr
+
+'''
