af6355aa97446619f03e18b910a793e9d3a3ac18
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-19 15:36:19 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -1,11 +1,16 @@
 def solution(num_list):
-    answer = 0
+    # 변수 정의 
+    cnt = 0
+    
+    # 연산 횟수 구하기
     for n in num_list:
-        while n != 1:
+        while n > 1:
+            
             if n % 2 == 0:
                 n = n // 2
-                answer += 1
             else:
                 n = (n-1)//2
-                answer += 1
-    return answer
+                
+            cnt += 1
+    # 출력
+    return cnt
