59baa403957dd585b2679acc0a0414624285314b
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-20 10:07:29 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.03 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/README.md
+++ 프로그래머스/0/120888. 중복된 문자 제거/README.md
@@ -1,10 +1,10 @@
 # [level 0] 중복된 문자 제거 - 120888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120888#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120888) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.01 ms
+메모리: 11.3 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 16일 08:46:05
+2026년 08월 20일 10:07:22
 
 ### 문제 설명
 
--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -1,4 +1,16 @@
 def solution(my_string):
+    # 변수 정의
+    arr = [i for i in my_string]
+    result = []
+    # 중복 문자열 제거 
+    for s in arr:
+        if s not in result:
+            result.append(s)
+    # 출력
+    return "".join(result)
+
+'''
+def solution(my_string):
     answer = ''
     for i in my_string:
         if i in answer :
@@ -6,3 +18,4 @@ def solution(my_string):
         else: 
             answer += i
     return answer
+'''
