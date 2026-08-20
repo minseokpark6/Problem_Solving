a6031208b3052f5905d00dc46b551a6732905ecf
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-20 09:46:28 +0900

    [level 0] Title: 배열 만들기 5, Time: 1.93 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
@@ -1,7 +1,12 @@
 def solution(intStrs, k, s, l):
+    return [int(i[s:s+l]) for i in intStrs if int(i[s:s+l]) > k]
+
+'''
+def solution(intStrs, k, s, l):
     answer = []
     for intStr in intStrs:
         ret = intStr[s:s+l]
         if int(ret) > k :
             answer.append(int(ret))
     return answer
+'''
