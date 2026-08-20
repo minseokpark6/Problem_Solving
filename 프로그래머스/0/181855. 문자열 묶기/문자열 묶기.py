abcf19300163f670acd69f3033426eb6a8265853
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-20 10:12:59 +0900

    [level 0] Title: 문자열 묶기, Time: 8.55 ms, Memory: 18.4 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -1,3 +1,21 @@
+def solution(strArr):
+    # 변수 정의 
+    length = {}
+    
+    # 길이가 같은 문자열 개수 카운트
+    for s in strArr:
+        l = len(s)
+        if l in length:
+            length[l] += 1
+        else:
+            length[l] = 1
+            
+    # 개수가 가장 많은 그룹의 크기 출력
+    return sorted(length.values())[-1]
+
+
+'''
+## 이전 정답 코드 
 from collections import Counter
 
 def solution(strArr):
@@ -6,7 +24,6 @@ def solution(strArr):
     answer = sorted(list(result.values()))[-1]
     return answer
 
-'''
 Pandas df에서의 value_count() >> list : Counter()
 
 1) 
