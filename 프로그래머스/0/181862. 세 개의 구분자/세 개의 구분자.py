c63efa23ca10787af11bb9a20447cbd0f0466bab
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-21 13:36:27 +0900

    [level 0] Title: 세 개의 구분자, Time: 5.97 ms, Memory: 22.5 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,6 +1,16 @@
 def solution(myStr):
+    # 구분자 변환
+    for sep in "abc":
+        myStr = myStr.replace(sep, " ")
+    
+    # 출력
+    return myStr.split() or ["EMPTY"]
+
+'''
+def solution(myStr):
     li = myStr.replace("a", " ").replace("b", " ").replace("c", " ").split(" ")
     answer = [s for s in li if s != ""]
     if len(answer) == 0:
         answer = ["EMPTY"]
     return answer
+'''
