1fe6bb24b8ce26e0fb0bc48f27ddd1e771d6f05d
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-25 19:03:27 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,3 +1,2 @@
 def solution(my_string, s, e):
-    answer = my_string[:s] + my_string[s:e+1][::-1] + my_string[e+1:]
-    return answer
+    return my_string[:s] + my_string[s:e+1][::-1] + my_string[e+1:]
