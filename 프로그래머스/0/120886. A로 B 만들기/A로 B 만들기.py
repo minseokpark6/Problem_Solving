056a5fc8ff69d12c112b5c023a3a966ef0fd284d
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-25 19:26:23 +0900

    [level 0] Title: A로 B 만들기, Time: 0.21 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,7 +1,12 @@
 def solution(before, after):
+    return 1 if sorted(before)==sorted(after) else 0
+    
+'''
+def solution(before, after):
     before = sorted(list(before))
     after = sorted(list(after))
     if before == after:
         return 1
     else :
         return 0
+'''
