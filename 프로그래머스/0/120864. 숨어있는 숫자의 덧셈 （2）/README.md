9cd406de3105c1f15815355962d774f97e2e4ac8
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-26 13:28:41 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/README.md
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 숨어있는 숫자의 덧셈 (2) - 120864 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120864#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120864) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.03 ms
+메모리: 11.5 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 20일 10:01:14
+2026년 08월 26일 13:28:35
 
 ### 문제 설명
 
--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,4 +1,14 @@
 def solution(my_string):
+    # 문자열 제거 
+    for s in my_string:
+        if s.isalpha():
+            my_string = my_string.replace(s, " ")
+    
+    # 숫자의 합 출력
+    return sum(int(n) for n in my_string.split(" ") if n)
+
+'''
+def solution(my_string):
     answer = 0
     num = 0
     for i in my_string:
@@ -9,3 +19,4 @@ def solution(my_string):
             num = 0
     answer += num
     return answer
+'''
