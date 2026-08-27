3bd1e258e762d317eaec7685ad931c8065834e98
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-27 12:10:53 +0900

    [level 0] Title: 가까운 수, Time: 0.02 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -1,4 +1,12 @@
 def solution(array, n):
+    # 정수 n과의 거리
+    diff = {i:abs(i-n) for i in array}
+    # 출력
+    return sorted(diff.items(), key= lambda x:(x[1], x[0]))[0][0]
+
+
+'''
+def solution(array, n):
     answer = array[0]
     temp = abs(array[0] - n)
     for i in array:
@@ -12,3 +20,4 @@ def solution(array, n):
                 continue
             
     return answer
+'''
