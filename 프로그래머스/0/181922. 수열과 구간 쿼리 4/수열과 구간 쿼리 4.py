d51435e7b717498ce9a7321719afbc230a704de2
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-28 19:09:47 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 5.64 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
@@ -1,4 +1,19 @@
 def solution(arr, queries):
+    # arr 변환하기 
+    for q in queries:
+        idx_list = list(range(q[0], q[1]+1))
+        for idx in idx_list:
+            if idx % q[2] == 0:
+                arr[idx] += 1
+    
+    # 출력
+    return arr
+                
+
+
+'''
+
+def solution(arr, queries):
     for q in queries:
         s, e, k = q[0], q[1], q[2]
         idx_list = list(range(s, e+1))
@@ -6,3 +21,4 @@ def solution(arr, queries):
             if idx % k == 0:
                 arr[idx] += 1
     return arr
+'''
