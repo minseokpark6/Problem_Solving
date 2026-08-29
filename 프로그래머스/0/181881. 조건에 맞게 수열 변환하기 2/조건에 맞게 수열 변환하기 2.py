e5b69f6778957f668d865bbf14587eea00a5dcad
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-29 13:14:16 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 310.75 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
@@ -1,4 +1,34 @@
 def solution(arr):
+    # 변수 정의
+    cnt = 0
+    
+    # 원소에 대한 작업 반복 
+    while True:
+        # 다음 회차 arr 리스트 생성
+        arr_next = []
+        
+        # arr 원소 작업
+        for n in arr:
+            if (n>=50) and (n%2==0):
+                arr_next.append(n//2)
+            elif (n<50) and (n%2!=0):
+                arr_next.append(2*n + 1)
+            else:
+                arr_next.append(n)
+        
+        # arr과 arr_next가 같은지 확인
+        if arr == arr_next:
+            return cnt 
+        
+        else:
+            cnt += 1
+            arr = arr_next
+        
+
+    
+
+'''
+def solution(arr):
     answer = 0
     while True:
         # 별도의 빈 리스트 생성
@@ -23,3 +53,4 @@ def solution(arr):
             answer += 1
             
     return answer
+'''
