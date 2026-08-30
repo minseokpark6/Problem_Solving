faece1734b3715825ca2e6e631dee0af201f7832
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-30 16:17:34 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.07 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,7 +1,23 @@
 def solution(my_string):
+    # 변수 정의
+    result = [0] * 52
+    
+    # 각 문자의 개수 찾기
+    for s in my_string:
+        if s.isupper():
+            result[ord(s) - ord('A')] += 1
+        else:
+            result[ord(s) - ord('a') + 26] += 1
+
+    # 출력
+    return result
+
+'''
+def solution(my_string):
     al = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     answer = [0] * len(al)
     for idx, a in enumerate(al):
         if a in my_string:
             answer[idx] += my_string.count(a)
     return answer
+'''
