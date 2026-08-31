9931f1585cdd77a2d520c041d9a9ee9dd9654ee4
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-31 17:10:26 +0900

    [level 0] Title: 조건 문자열, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -1,4 +1,17 @@
 def solution(ineq, eq, n, m):
+    # 조건 정의 
+    condition = {
+        ">=" : n>=m,
+        "<=" : n<=m,
+        ">!" : n>m,
+        "<!" : n<m
+    }
+    
+    # 출력
+    return 1 if condition[ineq+eq] else 0
+    
+'''
+def solution(ineq, eq, n, m):
     if ineq == "<" and eq == "=":
         result = (n <= m)
     elif ineq == ">" and eq == "=":
@@ -12,3 +25,5 @@ def solution(ineq, eq, n, m):
         return 1
     else:
         return 0
+
+'''
