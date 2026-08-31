7d5b2bd698ff8940c0223efde3e1d453021fc14d
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-31 16:45:54 +0900

    [level 0] Title: 소인수분해, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.py
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -1,4 +1,27 @@
 def solution(n):
+    # 변수 정의
+    result = []
+    i = 2
+    
+   # 소인수 구하기
+    while i * i <= n:
+        if n % i == 0:
+            result.append(i)
+            
+            # 중복 인수 제거
+            while n % i == 0:
+                n //= i
+
+        i += 1
+    if n > 1:
+        result.append(n)
+    # 출력
+    return result
+        
+    
+
+'''
+def solution(n):
     answer =  []
     d = 2
 
@@ -10,3 +33,4 @@ def solution(n):
             d += 1
     
     return sorted(list(set(answer)))
+'''
