bb372ff93e9b932361cd083a4e19fa14733b3b41
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-08-31 16:57:13 +0900

    [level 0] Title: 배열 만들기 4, Time: 18.81 ms, Memory: 15.1 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -1,4 +1,26 @@
 def solution(arr):
+    # 변수 정의
+    stk = []
+    i = 0
+    
+    # 새로운 배열 만들기
+    while i < len(arr):
+        if not stk:
+            stk.append(arr[i])
+            i += 1
+        else:
+            if stk[-1] < arr[i]:
+                stk.append(arr[i])
+                i += 1
+            else:
+                stk.pop()
+        
+    # 출력
+    return stk
+    
+
+'''
+def solution(arr):
     stk = []
     i = 0
     
@@ -15,3 +37,4 @@ def solution(arr):
             del stk[-1]
         
     return stk
+'''
