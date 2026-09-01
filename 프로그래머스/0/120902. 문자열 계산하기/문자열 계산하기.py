a49f165778bb713adb29b47a3761d39b01fc6d17
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-09-01 12:08:21 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,4 +1,22 @@
 def solution(my_string):
+    # 변수 정의
+    arr = my_string.split()
+    result = int(arr[0])
+
+    # 계산하기
+    for i in range(1, len(arr), 2):
+        if arr[i] == "-":
+            result -= int(arr[i+1])
+        else:
+            result += int(arr[i+1])
+
+    # 출력
+    return result
+
+
+
+'''
+def solution(my_string):
     sum = 0
     temp = my_string.split(" ")
     answer = []
@@ -12,3 +30,4 @@ def solution(my_string):
     for i in answer:
         sum += i
     return sum
+'''
