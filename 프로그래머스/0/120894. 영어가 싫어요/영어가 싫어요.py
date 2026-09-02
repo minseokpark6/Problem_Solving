d253b0afdc07f3a95c35209815d0c4df8c5aa9eb
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-09-02 15:26:05 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,5 +1,27 @@
 def solution(numbers):
+    # 변수 정의
+    nums = {"zero":"0", 
+        "one":"1", 
+        "two":"2", 
+        "three":"3", 
+        "four":"4", 
+        "five":"5", 
+        "six":"6", 
+        "seven":"7", 
+        "eight":"8", 
+        "nine":"9"}
+    # 정수로 변환
+    for word, digit in nums.items():
+        numbers = numbers.replace(word, digit)
+        
+    # 출력
+    return int(numbers)
+
+
+'''
+def solution(numbers):
     nums = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     for idx, num in enumerate(nums):
         numbers = numbers.replace(num, str(idx))
     return int(numbers)
+'''
