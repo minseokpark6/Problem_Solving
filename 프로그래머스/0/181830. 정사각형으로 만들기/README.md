fd8effeebc8d4bf8f951965346dad1bff9eaf176
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-09-04 11:59:24 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.09 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/README.md
+++ 프로그래머스/0/181830. 정사각형으로 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 정사각형으로 만들기 - 181830 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181830#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181830) 
 
 ### 성능 요약
 
-메모리: 11 MB, 시간: 0.16 ms
+메모리: 11.7 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 30일 10:49:23
+2026년 09월 04일 11:59:18
 
 ### 문제 설명
 
--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.py
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.py
@@ -1,4 +1,19 @@
 def solution(arr):
+    # 변수 정의 
+    x, y = len(arr[0]), len(arr)
+
+    # 이차원 배열에 0 추가 후 출력
+    if x < y:
+        return [i + [0]*(y-x) for i in arr]
+    elif x > y:
+        return arr + [[0] * x] * (x - y)
+    else:
+        return arr
+
+
+
+'''
+def solution(arr):
     # 이차원 리스트의 (x, y)값 비교
     for i in range(len(arr)):
         # y값이 클 경우 > 2차원 리스트 원소인 1차원 리스트에 0값 추가
@@ -10,3 +25,4 @@ def solution(arr):
             arr = arr + [[0] * len(arr[i])] * (len(arr[i]) - len(arr))
              
     return arr
+'''
