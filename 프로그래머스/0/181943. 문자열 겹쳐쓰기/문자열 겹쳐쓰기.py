1c68e678f7bcce88e59b6ec63dde4a34cfb822f0
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-09-04 11:48:45 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,5 +1,2 @@
 def solution(my_string, overwrite_string, s):
-    a = my_string[:s]
-    b = my_string[(s + len(overwrite_string)):]
-    answer = a + overwrite_string + b
-    return answer
+    return my_string[:s] + overwrite_string + my_string[s+len(overwrite_string):]
