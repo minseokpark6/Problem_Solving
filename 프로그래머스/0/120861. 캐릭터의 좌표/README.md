0d70df773e4c7dd5c409437eb81e35b639d18e86
Park Minseok <126630533+minseokpark6@users.noreply.github.com>
2026-09-05 19:12:24 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/README.md
+++ 프로그래머스/0/120861. 캐릭터의 좌표/README.md
@@ -1,10 +1,10 @@
 # [level 0] 캐릭터의 좌표 - 120861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120861#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120861) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.03 ms
+메모리: 11.6 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 19일 15:41:02
+2026년 09월 05일 19:12:17
 
 ### 문제 설명
 
--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,4 +1,25 @@
 def solution(keyinput, board):
+    # 좌표 시작점 및 맵 크기 정의 
+    x, y = 0, 0
+    x_max, y_max = board[0]//2, board[1]//2
+
+    # 좌표 이동
+    for key in keyinput:
+        if (key == 'left') and (x != -(x_max)):
+            x -= 1
+        elif (key == 'right') and (x != x_max):
+            x += 1
+        elif (key == 'up') and (y != y_max):
+            y += 1
+        elif (key == 'down') and (y != -(y_max)):
+            y -= 1
+
+    # 출력
+    return [x, y]
+
+
+'''
+def solution(keyinput, board):
     answer = [0, 0]
     for i in keyinput:
         x_max = board[0] // 2
@@ -25,3 +46,4 @@ def solution(keyinput, board):
                 answer[0] += 1
         
     return answer
+'''
